6e238105b81db6232cbd2507d49fa1b6ea272ccd
dlohvinov <nsblnr@gmail.com>
2019-07-29 14:02:35 +0300

    refactor: added module-wrap component |WTEL-831|
    added module-wrap component with default module elements (like alert, navigation and header) to avoid code repeats and localize control over these components

---
+++ src/components/modules/module-wrap.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div class="module-wrap">
+        <app-nav></app-nav>
+        <main class="module permissions">
+            <alert
+                    :error="error"
+            ></alert>
+            <app-header></app-header>
+
+            <slot></slot>
+
+        </main>
+    </div>
+</template>
+
+<script>
+    import theHeader from '../the-header';
+    import theNavbar from '../the-navbar';
+    import alert from '../utils/alert';
+
+    import errorMixin from '../../mixins/errorMixin';
+    export default {
+        name: "module-wrap",
+        components: {
+            'app-header': theHeader,
+            'app-nav': theNavbar,
+            alert,
+        },
+        mixins: [errorMixin],
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "../../assets/css/modules/modules";
+</style>
