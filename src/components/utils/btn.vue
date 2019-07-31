a60693441f7309b60a5a2be7fd635e1542fc6a0a
dlohvinov <nsblnr@gmail.com>
2019-07-31 17:00:15 +0300

    dev: add ripple lib, reworked buttons due to styleguide |WTEL-834|

--- src/components/utils/btn.vue
+++ src/components/utils/btn.vue
@@ -1,22 +1,29 @@
 <template>
     <button
-            class="btn fs14-lh16"
-            v-bind:type="type"
+            class="btn"
+            ref="btn"
+            v-ripple
+            :type="type"
     >
         <slot></slot>
     </button>
 </template>
 
 <script>
-export default {
-  name: 'btn',
-  props: {
-    type: {
-      type: String,
-      default: 'button',
-    },
-  },
-};
+    import Vue from 'vue';
+    import Ripple from '../../assets/lib/ripple/ripple';
+
+    Vue.directive('ripple', Ripple);
+
+    export default {
+        name: 'btn',
+        props: {
+            type: {
+                type: String,
+                default: 'button',
+            },
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
@@ -33,14 +40,44 @@ export default {
         color: #000;
         background: $accent-color;
         border-radius: 4px;
+        transition: 0.2s;
 
         border: 1px solid transparent;
         outline: none;
+
+        &:hover {
+            background: #FED546;
+        }
+
+        &:active {
+            background: $accent-color;
+        }
+
+        &:disabled {
+            background: rgba(0, 0, 0, 0.3);
+        }
+
+
+        &.secondary-btn {
+            color: #000;
+            background: transparent;
+            border: 1px solid #000;
+
+            &:hover {
+                color: rgba(0, 0, 0, 0.3);
+                border-color: rgba(0, 0, 0, 0.3);
+            }
+
+            &:active {
+                color: #000;
+                border: 1px solid #000;
+            }
+
+            &:disabled {
+                color: rgba(0, 0, 0, 0.3);
+                border-color: rgba(0, 0, 0, 0.3);
+            }
+        }
     }
 
-    .secondary-btn {
-        color: rgba(0, 0, 0, 0.3);
-        background: transparent;
-        border: 1px solid rgba(0, 0, 0, 0.3);
-    }
 </style>
