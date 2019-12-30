a476c087ab82d25d7ec0926cdbe3cdec5af1aed9
dlohvinov <nsblnr@gmail.com>
2019-12-30 11:25:13 +0200

    dev: buckets connected to backend [WTEL-1003]

--- src/components/objects/contact-center/buckets/opened-bucket.vue
+++ src/components/objects/contact-center/buckets/opened-bucket.vue
@@ -1,53 +1,55 @@
 <template>
     <div class="content-wrap">
         <object-header
-                :primaryText="$t('objects.save')"
-                :primaryAction="submit"
+                :primaryText="computePrimaryText"
+                :primaryAction="save"
+                :primaryDisabled="computeDisabled"
                 close
         >
-            <span>{{$tc('objects.ccenter.buckets.buckets', 1)}}</span> | {{computeTitle}}
+            {{$tc('objects.ccenter.buckets.buckets', 1)}} |
+            {{computeTitle}}
         </object-header>
-        <section class="object-content module-new">
-            <header class="content-header">
-                <h3 class="content-title">{{$t('objects.generalInfo')}}</h3>
-            </header>
-
-            <form class="new_w50">
-                <form-input
-                        v-model.trim="$v.itemInstance.name.$model"
-                        :v="$v.itemInstance.name"
-                        :label="$t('objects.name')"
-                        required
-                ></form-input>
-
-                <form-input
-                        v-model.trim="itemInstance.description"
-                        :label="$t('objects.description')"
-                        textarea
-                ></form-input>
-
-            </form>
-        </section>
+        <tabs-component
+                :tabs="tabs"
+                :root="$options.name"
+        >
+            <template slot="component" slot-scope="props">
+                <component
+                        class="tabs-inner-component"
+                        :is="props.currentTab"
+                        :v="$v"
+                ></component>
+            </template>
+        </tabs-component>
     </div>
 </template>
 
 <script>
+    import openedBucketGeneral from './opened-bucket-general';
     import editComponentMixin from '@/mixins/editComponentMixin';
     import {required} from 'vuelidate/lib/validators';
+    import {mapActions, mapState} from "vuex";
 
     export default {
-        name: "opened-bucket-skill",
+        name: "opened-bucket",
         mixins: [editComponentMixin],
+        components: {openedBucketGeneral},
+
         data() {
             return {
-                itemInstance: {
-                    name: 'bucket name',
-                    // description: '',
-                },
+                tabs: [
+                    {
+                        text: this.$t('objects.general'),
+                        value: 'general',
+                    },
+                    {
+                        text: this.$tc('objects.permissions.permissions'),
+                        value: 'permissions-tab',
+                    },
+                ],
             };
         },
 
-        // by vuelidate
         validations: {
             itemInstance: {
                 name: {
@@ -56,17 +58,24 @@
             }
         },
 
-        methods: {
-            save() {
-                if (this.id) {
-                    //    update
-                } else {
-                    //    create
-                }
-
-                this.close();
+        computed: {
+            ...mapState('ccenter/buckets', {
+                itemInstance: state => state.itemInstance,
+            }),
+            id: {
+                get() {return this.$store.state.ccenter.buckets.itemId},
+                set(value) {this.setId(value)}
             }
-        }
+        },
+
+        methods: {
+            ...mapActions('ccenter/buckets', {
+                setId: 'SET_ITEM_ID',
+                loadItem: 'LOAD_ITEM',
+                addItem: 'ADD_ITEM',
+                updateItem: 'UPDATE_ITEM',
+            }),
+        },
     }
 </script>
 
--- src/components/objects/contact-center/buckets/the-buckets.vue
+++ src/components/objects/contact-center/buckets/the-buckets.vue
@@ -41,6 +41,12 @@
                         ></i>
                 </template>
             </vuetable>
+            <pagination
+                    v-model="size"
+                    @loadDataList="loadDataList"
+                    @next="nextPage"
+                    @prev="prevPage"
+            ></pagination>
         </section>
     </div>
 </template>
@@ -48,20 +54,35 @@
 <script>
     import tableComponentMixin from '@/mixins/tableComponentMixin';
     import {_actionsTableField_2} from "../../../../utils/tableFieldPresets";
+    import {mapActions, mapState} from "vuex";
 
     export default {
         name: "the-agent-buckets",
         mixins: [tableComponentMixin],
         data() {
             return {
-                dataList: [],
-                // vuetable prop
                 fields: [
                     {name: 'name', title: this.$t('objects.name')},
                     {name: 'description', title: this.$t('objects.description')},
                     _actionsTableField_2
                 ],
             };
+        },
+
+        computed: {
+            ...mapState('ccenter/buckets', {
+                dataList: state => state.dataList,
+            }),
+
+            size: {
+                get() {return this.$store.state.ccenter.buckets.size},
+                set(value) {this.setSize(value)}
+            },
+
+            search: {
+                get() {return this.$store.state.ccenter.buckets.search},
+                set(value) {this.setSearch(value)}
+            },
         },
 
         methods: {
@@ -76,19 +97,14 @@
                 });
             },
 
-            remove(rowId) {
-                this.dataList.splice(rowId, 1);
-            },
-
-            loadDataList() {
-                for (let i = 0; i < 10; i++) {
-                    this.dataList.push({
-                        name: 'bucket name ' + i,
-                        description: 'Description',
-                        id: i
-                    });
-                }
-            }
+            ...mapActions('ccenter/buckets', {
+                loadDataList: 'LOAD_DATA_LIST',
+                setSize: 'SET_SIZE',
+                setSearch: 'SET_SEARCH',
+                nextPage: '',
+                prevPage: '',
+                removeItem: 'REMOVE_ITEM',
+            }),
         }
     }
 </script>
