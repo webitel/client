9dc10bc02f829843f5bce636b2bc542234b0d047
dlohvinov <nsblnr@gmail.com>
2020-02-14 10:06:02 +0200

    dev: updated dashboard info and rss wrap design [WTEL-1076]

--- src/components/home/the-home.vue
+++ src/components/home/the-home.vue
@@ -34,53 +34,47 @@
             </article>
         </section>
 
-        <section class="object-content">
-            <section class="dashboard__info-section">
-                <div class="dashboard__info-wrap">
-                    <header class="content-header">
-                        <div class="logo-wrap">
-                            <img src="../../assets/img/logo-dark.svg" alt="logo" class="logo">
-                        </div>
-                    </header>
-                    <p class="dashboard__info-text">
-                        <!--                        {{$t('home.welcomeText')}}-->
-                        <b>Cloud vs On-Site: </b>Security policy does not allow to store data and use cloud services?
-                        With Webitel, you can build a contact center on your site!
-                        <br><br><b>Multichannel vs Omni-Channel: </b>Be always and everywhere in touch with your
-                        customers! IVR-menu of any complexity, outbound, inbound interaction campaigns or message on
-                        social networks - now it's easier than ever!
-                        <br><br><b>Fault-tolerant infrastructure: </b>No more unplanned downtime. Fast scaling and
-                        fault-tolerant architecture allows you to simultaneously serve more than 1000 customer calls per
-                        minute from different communication channels.
-                        <br><br><b>Design: </b>The new minimalistic interface with the most user-friendly admin panel
-                        that allows you to have quick access to all the necessary functions. Less clicks - more
-                        features.
-                        <br><br><b>New agent group work module: </b>The most efficient call distribution. An agent can
-                        have several skills at once for participating in different campaigns. The client is served only
-                        by professionals.
-                        <br><br><b>Setting Permissions: </b>New permissions management module - new opportunities for
-                        control. Users can make changes only to sections, as well as to actions.
-
-                    </p>
-                </div>
-                <aside class="dashboard__rss-wrap">
-                    <h2 class="dashboard__rss-title">{{rssTitle}}</h2>
-                    <div
-                            class="dashboard__rss-item"
+        <section class="dashboard-info">
+            <article class="dashboard-info__article">
+                <p>
+                    <!-- {{$t('home.welcomeText')}}-->
+                    <b>Cloud vs On-Site: </b>Security policy does not allow to store data and use cloud services?
+                    With Webitel, you can build a contact center on your site!
+                    <br><br><b>Multichannel vs Omni-Channel: </b>Be always and everywhere in touch with your
+                    customers! IVR-menu of any complexity, outbound, inbound interaction campaigns or message on
+                    social networks - now it's easier than ever!
+                    <br><br><b>Fault-tolerant infrastructure: </b>No more unplanned downtime. Fast scaling and
+                    fault-tolerant architecture allows you to simultaneously serve more than 1000 customer calls per
+                    minute from different communication channels.
+                    <br><br><b>Design: </b>The new minimalistic interface with the most user-friendly admin panel
+                    that allows you to have quick access to all the necessary functions. Less clicks - more
+                    features.
+                    <br><br><b>New agent group work module: </b>The most efficient call distribution. An agent can
+                    have several skills at once for participating in different campaigns. The client is served only
+                    by professionals.
+                    <br><br><b>Setting Permissions: </b>New permissions management module - new opportunities for
+                    control. Users can make changes only to sections, as well as to actions.
+                </p>
+            </article>
+            <aside class="dashboard-rss">
+                <h3 class="dashboard-rss__title">{{rssTitle}}</h3>
+                <ul class="dashboard-rss__list">
+                    <li
+                            class="dashboard-rss__list-item"
                             v-for="(item, key) of rssFeed"
                             :key="key"
                     >
                         {{item.title}}:
                         <a
-                                class="dashboard__rss-link"
+                                class="dashboard-rss__list-item-link"
                                 :href="item.link"
                                 target="_blank"
                         >
                             {{$t('home.rssLink')}}
                         </a>
-                    </div>
-                </aside>
-            </section>
+                    </li>
+                </ul>
+            </aside>
         </section>
     </div>
 </template>
@@ -208,15 +202,17 @@
 </script>
 
 <style lang="scss" scoped>
+    $scrollbar-bg-color: #EAEAEA;
+
     .charts-wrap {
         display: grid;
         grid-template-columns: repeat(auto-fit, minmax(340px, 1fr));
-        grid-column-gap: 28px;
-        grid-row-gap: 28px;
+        grid-gap: 28px;
 
         .chart {
             padding: 33px 41px;
             background: #fff;
+            border-radius: $border-radius;
 
             .chart-title {
                 @extend .typo-heading-md;
@@ -225,38 +221,61 @@
         }
     }
 
-    .dashboard__info-section {
+    .dashboard-info {
         display: grid;
-        grid-template-columns: 2fr 1fr;
-        grid-column-gap: 28px;
+        grid-template-columns: calc((100% * 2 - 28px) / 3) 1fr; // calc(2fr - 28px grid gap)
+        grid-gap: 28px;
+        margin-top: 28px;
 
-        .dashboard__info-wrap {
-
-            .logo-wrap {
-                display: inline-block;
-                margin: auto;
-            }
-
-            .dashboard__info-text {
-                margin-top: 28px;
-            }
+        .dashboard-info__article {
+            padding: 32px 41px;
+            background: #fff;
+            border-radius: $border-radius;
         }
 
-        .dashboard__rss-wrap {
-            .dashboard__rss-title {
+        .dashboard-rss {
+            position: relative;
+            min-height: 400px;
+            padding: 32px 41px;
+            background: #fff;
+            border-radius: $border-radius;
+
+            .dashboard-rss__title {
                 @extend .typo-heading-md;
-                margin-bottom: 28px;
+                margin: 0 0 28px;
             }
 
-            .dashboard__rss-item {
-                @extend .typo-body-sm;
-                margin-bottom: 14px;
-            }
+            .dashboard-rss__list {
+                @extend .typo-body-md;
+                position: absolute;
+                top: calc(32px + 22px + 28px); // top padding + h3 line height + h3 margin-bottom
+                left: 41px;
+                right: 41px;
+                bottom: 32px;
+                padding-right: 41px;
+                overflow-y: auto;
 
-            .dashboard__rss-link {
-                @extend .typo-body-sm;
+                // scrollbar itself
+                &::-webkit-scrollbar {
+                    width: 4px;
+                    background-color: $scrollbar-bg-color;
+                    cursor: pointer;
+                }
 
-                color: #000;
+                // scrollable element
+                &::-webkit-scrollbar-thumb {
+                    width: 4px;
+                    background-color: $accent-color;
+                    border-radius: $border-radius;
+                }
+
+                &-item {
+                    margin-bottom: 14px;
+
+                    &-link {
+                        color: #000;
+                    }
+                }
             }
         }
     }
