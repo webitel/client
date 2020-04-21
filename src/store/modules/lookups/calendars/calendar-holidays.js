72c57eed125ce33235acfd5025264c5f07795e80
pvoroniak <p.voroniak@webitel.com>
2020-04-21 11:58:13 +0300

    dev: use new default in holidays and member communications

--- src/store/modules/lookups/calendars/calendar-holidays.js
+++ src/store/modules/lookups/calendars/calendar-holidays.js
@@ -1,9 +1,8 @@
-import proxy from '../../../../utils/editProxy';
 import store from '../../../store';
+import {DefaultNestedListModule} from "../../defaults/DefaultNestedListModule";
 
 const defaultState = () => {
     return {
-        itemId: null,
         itemInstance: {
             name: '',
             date: Date.now(),
@@ -12,9 +11,10 @@ const defaultState = () => {
     };
 };
 
+const defaultModule = new DefaultNestedListModule(defaultState);
+
 const state = {
-    dataList: [],
-    ...defaultState()
+    ...defaultModule.state,
 };
 
 const getters = {};
@@ -53,79 +53,13 @@ const actions = {
             id, {root: true});
     },
 
-    SET_ITEM_ID: (context, id) => {
-        if (id !== 'new') context.commit('SET_ITEM_ID', id);
-    },
-
-    LOAD_DATA_LIST: async (context) => {
-        const response = await context.dispatch('GET_LIST');
-        context.dispatch('RESET_ITEM_STATE');
-        context.commit('SET_DATA_LIST', response);
-    },
-
-    LOAD_ITEM: async (context) => {
-        if (typeof state.itemId === 'number') {
-            const item = await context.dispatch('GET_ITEM');
-            context.commit('SET_ITEM', proxy(item));
-        }
-    },
-
-    SET_ITEM_PROPERTY: (context, payload) => {
-        context.commit('SET_ITEM_PROPERTY', payload);
-    },
-
-    ADD_ITEM: async (context) => {
-        if (typeof state.itemId !== 'number') {
-            await context.dispatch('POST_ITEM');
-        }
-        context.dispatch('LOAD_DATA_LIST');
-    },
-
-    UPDATE_ITEM: async (context) => {
-        if (state.itemInstance._dirty) {
-            await context.dispatch('UPD_ITEM');
-        }
-        context.dispatch('LOAD_DATA_LIST');
-    },
-
-    REMOVE_ITEM: async (context, index) => {
-        context.commit('REMOVE_ITEM', index);
-        try {
-            await context.dispatch('DELETE_ITEM', index);
-        } catch {
-        }
-        context.dispatch('LOAD_DATA_LIST');
-    },
-
-    RESET_ITEM_STATE: async (context) => {
-        context.commit('RESET_ITEM_STATE');
-    },
+    ...defaultModule.actions,
 };
 
 const mutations = {
-    SET_ITEM_ID: (state, id) => {
-        state.itemId = id;
-    },
 
-    SET_DATA_LIST: (state, list) => {
-        state.dataList = list;
-    },
+    ...defaultModule.mutations,
 
-    SET_ITEM_PROPERTY: (state, {prop, value}) => {
-        state.itemInstance[prop] = value;
-    },
-
-    SET_ITEM: (state, item) => {
-        state.itemInstance = item;
-    },
-
-    REMOVE_ITEM: (state, index) => {
-        state.dataList.splice(index, 1);
-    },
-
-    RESET_ITEM_STATE: (state) => {
-        Object.assign(state, defaultState());
-    },
 };
 
 export default {
