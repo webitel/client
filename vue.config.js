871850203341cdb8003f43a397601c07ca364e76
dlohvinov <nsblnr@gmail.com>
2020-11-13 16:23:26 +0200

    changed publicPath folder and routing [WTEL-1532]

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,8 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+    // publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+    publicPath: '/',
     lintOnSave: true,
     css: {
         loaderOptions: {
