599779f404e6804be303c0d063b56df3c203e5c3
dlohvinov <nsblnr@gmail.com>
2020-12-28 20:18:07 +0200

    fix: updated inbound queue timing tab, added originateTimeout field [WTEL-1629]

--- src/components/contact-center/queues/inbound-queue/opened-queue-inbound-queue-timing.vue
+++ src/components/contact-center/queues/inbound-queue/opened-queue-inbound-queue-timing.vue
@@ -1,85 +1,78 @@
 <template>
-    <section>
-        <header class="content-header">
-            <h3 class="content-title">{{$t('objects.ccenter.queues.timing')}}</h3>
-        </header>
-        <form class="object-input-grid">
-                <form-input
-                        v-model="discardAbandonedAfter"
-                        :label="$t('objects.ccenter.queues.discardAbandonedAfter')"
-                ></form-input>
-                <form-input
-                        v-model="timeoutWithNoAgents"
-                        :label="$t('objects.ccenter.queues.timeoutWithNoAgents')"
-                ></form-input>
+  <section>
+    <header class="content-header">
+      <h3 class="content-title">{{ $t('objects.ccenter.queues.timing') }}</h3>
+    </header>
+    <form class="object-input-grid">
+      <wt-timepicker
+          :value="originateTimeout"
+          :label="$t('objects.ccenter.queues.originateTimeout')"
+          @input="setItemPayloadProp({ prop: 'originateTimeout', value: $event })"
+      ></wt-timepicker>
+      <wt-timepicker
+          :value="discardAbandonedAfter"
+          :label="$t('objects.ccenter.queues.discardAbandonedAfter')"
+          @input="setItemPayloadProp({ prop: 'discardAbandonedAfter', value: $event })"
+      ></wt-timepicker>
+      <wt-timepicker
+          :value="timeoutWithNoAgents"
+          :label="$t('objects.ccenter.queues.timeoutWithNoAgents')"
+          @input="setItemPayloadProp({ prop: 'timeoutWithNoAgents', value: $event })"
+      ></wt-timepicker>
+      <wt-select
+          :value="timeBaseScore"
+          :options="dropdownOptionsScoreList"
+          :label="$t('objects.ccenter.queues.timeBaseScore')"
+          :track-by="null"
+          @input="setItemPayloadProp({ prop: 'timeBaseScore', value: $event })"
+      ></wt-select>
 
-                <dropdown-select
-                        v-model="timeBaseScore"
-                        :options="dropdownOptionsScoreList"
-                        :label="$t('objects.ccenter.queues.timeBaseScore')"
-                ></dropdown-select>
-
-        </form>
-    </section>
+    </form>
+  </section>
 </template>
 
 <script>
-    import timepicker from '@/components/utils/timepicker';
-    import openedTabComponentMixin from '@/mixins/openedTabComponentMixin';
-    import {mapActions} from "vuex";
-    import DropdownSelect from "../../../utils/dropdown-select";
+import { mapState, mapActions } from 'vuex';
+import openedTabComponentMixin from '../../../../mixins/openedTabComponentMixin';
 
-    export default {
-        name: 'opened-queue-inbound-queue-timing',
-        mixins: [openedTabComponentMixin],
-        components: {DropdownSelect, timepicker},
-        data() {
-            return {
-                dropdownOptionsScoreList: ['score', 'system'],
-            }
-        },
+export default {
+  name: 'opened-queue-inbound-queue-timing',
+  mixins: [openedTabComponentMixin],
+  data: () => ({
+    dropdownOptionsScoreList: ['score', 'system'],
+  }),
+  computed: {
+    ...mapState('ccenter/queues', {
+      originateTimeout: (state) => state.itemInstance.payload.originateTimeout,
+      timeBaseScore: (state) => state.itemInstance.payload.timeBaseScore,
+    }),
+    discardAbandonedAfter: {
+      get() {
+        return this.$store.state.ccenter.queues.itemInstance.payload.discardAbandonedAfter;
+      },
+      set(value) {
+        this.setItemPayloadProp({ prop: 'discardAbandonedAfter', value: +value });
+      },
+    },
 
-        computed: {
-            discardAbandonedAfter: {
-                get() {
-                    return this.$store.state.ccenter.queues.itemInstance.payload.discardAbandonedAfter;
-                },
-                set(value) {
-                    this.setItemPayloadProp({ prop: 'discardAbandonedAfter', value: +value });
-                },
-            },
+    timeoutWithNoAgents: {
+      get() {
+        return this.$store.state.ccenter.queues.itemInstance.payload.timeoutWithNoAgents;
+      },
+      set(value) {
+        this.setItemPayloadProp({ prop: 'timeoutWithNoAgents', value: +value });
+      },
+    },
+  },
 
-            timeoutWithNoAgents: {
-                get() {
-                    return this.$store.state.ccenter.queues.itemInstance.payload.timeoutWithNoAgents;
-                },
-                set(value) {
-                    this.setItemPayloadProp({ prop: 'timeoutWithNoAgents', value: +value });
-                },
-            },
-
-            timeBaseScore: {
-                get() {
-                    return this.$store.state.ccenter.queues.itemInstance.payload.timeBaseScore;
-                },
-                set(value) {
-                    this.setItemPayloadProp({ prop: 'timeBaseScore', value: +value });
-                },
-            },           
-
-        },
-
-        methods: {
-            ...mapActions('ccenter/queues', {
-                setItemProp: 'SET_ITEM_PROPERTY',
-                setItemPayloadProp: 'SET_ITEM_PAYLOAD_PROPERTY',
-            }),
-        },
-    };
+  methods: {
+    ...mapActions('ccenter/queues', {
+      setItemProp: 'SET_ITEM_PROPERTY',
+      setItemPayloadProp: 'SET_ITEM_PAYLOAD_PROPERTY',
+    }),
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    .timepicker {
-        margin-bottom: 28px;
-    }
 </style>
