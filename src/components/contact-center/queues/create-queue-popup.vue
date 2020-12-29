23f4248fa9afee9fbf27d30366eda23c716b898c
dlohvinov <nsblnr@gmail.com>
2020-12-29 18:35:00 +0200

    refactor: refactored queue select popup [WTEL-1639]

--- src/components/contact-center/queues/create-queue-popup.vue
+++ src/components/contact-center/queues/create-queue-popup.vue
@@ -1,173 +1,154 @@
 <template>
-    <popup
-        :title="$t('objects.ccenter.queues.newQueue')"
-        :primaryText="$t('objects.create')"
-        :primaryAction="createItemInstance"
-        :disableAction="!!this.selectedOption"
-        @close="$emit('close')"
-    >
-        <section>
-            <p class="popup-subheading">{{$t('objects.ccenter.queues.newQueueDescription')}}</p>
+  <wt-popup class="create-queue-popup" @close="close">
+    <template slot="title">{{ $t('objects.ccenter.queues.newQueue') }}</template>
 
-            <ul class="popup-options">
+    <template slot="main">
+      <ul class="popup-options">
+        <li
+            class="popup-options__item-wrap"
+            v-for="(option, key) of options"
+            :class="{'active': option === selected}"
+            :key="key"
+            @click="selectOption(option)"
+        >
+          <h4 class="popup-options__item-header">{{ option.title }}</h4>
+          <wt-icon-btn
+              icon="rounded-info"
+              color="outline"
+              :tooltip="option.description"
+          ></wt-icon-btn>
+        </li>
+      </ul>
+    </template>
 
-                <li
-                        class="popup-options__item-wrap"
-                        :class="{'active': computeSelectedOption('outbound-ivr')}"
-                        @click="selectPopupOption('outbound-ivr')"
-                >
-                    <h4 class="popup-options__item-header">
-                        {{$t('objects.ccenter.queues.outboundIVR')}}
-                    </h4>
-                    <p class="popup-options__item-text">
-                        {{$t('objects.ccenter.queues.outboundIVRDescription')}}
-                    </p>
-                </li>
-
-                <li
-                        class="popup-options__item-wrap"
-                        :class="{'active': computeSelectedOption('inbound-queue')}"
-                        @click="selectPopupOption('inbound-queue')"
-                >
-                    <h4 class="popup-options__item-header">
-                        {{$t('objects.ccenter.queues.inboundQueue')}}
-                    </h4>
-                    <p class="popup-options__item-text">
-                        {{$t('objects.ccenter.queues.inboundQueueDescription')}}
-                    </p>
-                </li>
-
-                <li
-                        class="popup-options__item-wrap"
-                        :class="{'active': computeSelectedOption('offline-queue')}"
-                        @click="selectPopupOption('offline-queue')"
-                >
-                    <h4 class="popup-options__item-header">
-                        {{$t('objects.ccenter.queues.offlineQueue')}}
-                    </h4>
-                    <p class="popup-options__item-text">
-                        {{$t('objects.ccenter.queues.offlineQueueDescription')}}
-                    </p>
-                </li>
-
-                <li
-                        class="popup-options__item-wrap"
-                        :class="{'active': computeSelectedOption('preview-dialer')}"
-                        @click="selectPopupOption('preview-dialer')"
-                >
-                    <h4 class="popup-options__item-header">
-                        {{$t('objects.ccenter.queues.previewDialer')}}
-                    </h4>
-                    <p class="popup-options__item-text">
-                        {{$t('objects.ccenter.queues.previewDialerDescription')}}
-                    </p>
-                </li>
-
-                <li
-                        class="popup-options__item-wrap"
-                        :class="{'active': computeSelectedOption('progressive-dialer')}"
-                        @click="selectPopupOption('progressive-dialer')"
-                >
-                    <h4 class="popup-options__item-header">
-                        {{$t('objects.ccenter.queues.progressiveDialer')}}
-                    </h4>
-                    <p class="popup-options__item-text">
-                        {{$t('objects.ccenter.queues.progressiveDialerDescription')}}
-                    </p>
-                </li>
-
-                <li
-                        class="popup-options__item-wrap"
-                        :class="{'active': computeSelectedOption('predictive-dialer')}"
-                        @click="selectPopupOption('predictive-dialer')"
-                >
-                    <h4 class="popup-options__item-header">
-                        {{$t('objects.ccenter.queues.predictiveDialer')}}
-                    </h4>
-                    <p class="popup-options__item-text">
-                        {{$t('objects.ccenter.queues.predictiveDialerDescription')}}
-                    </p>
-                </li>
-            </ul>
-        </section>
-    </popup>
+    <template slot="actions">
+      <wt-button
+          :disabled="!selected"
+          @click="createQueue"
+      >{{ $t('objects.create') }}
+      </wt-button>
+      <wt-button
+          color="secondary"
+          @click="close"
+      >{{ $t('objects.close') }}
+      </wt-button>
+    </template>
+  </wt-popup>
 </template>
 
 <script>
-    import popup from '@/components/utils/popup';
+export default {
+  name: 'create-queue-popup',
 
-    export default {
-        name: 'create-queue-popup',
-        components: {
-            popup,
-        },
+  data: () => ({
+    selected: null,
+  }),
 
-        data() {
-            return {
-                selectedOption: '',
-            };
-        },
+  computed: {
+    options() {
+      const outboundIVR = {
+        value: 'outbound-ivr',
+        title: this.$t('objects.ccenter.queues.outboundIVR'),
+        description: this.$t('objects.ccenter.queues.outboundIVRDescription'),
+      };
+      const inbound = {
+        value: 'inbound-queue',
+        title: this.$t('objects.ccenter.queues.inboundQueue'),
+        description: this.$t('objects.ccenter.queues.inboundQueueDescription'),
+      };
+      const offline = {
+        value: 'offline-queue',
+        title: this.$t('objects.ccenter.queues.offlineQueue'),
+        description: this.$t('objects.ccenter.queues.offlineQueueDescription'),
+      };
+      const previewDialer = {
+        value: 'preview-dialer',
+        title: this.$t('objects.ccenter.queues.previewDialer'),
+        description: this.$t('objects.ccenter.queues.previewDialerDescription'),
+      };
+      const progressiveDialer = {
+        value: 'progressive-dialer',
+        title: this.$t('objects.ccenter.queues.progressiveDialer'),
+        description: this.$t('objects.ccenter.queues.progressiveDialerDescription'),
+      };
+      const predictiveDialer = {
+        value: 'predictive-dialer',
+        title: this.$t('objects.ccenter.queues.predictiveDialer'),
+        description: this.$t('objects.ccenter.queues.predictiveDialerDescription'),
+      };
+      return [outboundIVR, inbound, offline, previewDialer, progressiveDialer, predictiveDialer];
+    },
+  },
 
+  methods: {
+    close() {
+      this.$emit('close');
+    },
 
-        methods: {
-            selectPopupOption(option) {
-                this.selectedOption = option;
-            },
+    selectOption(option) {
+      this.selected = option;
+    },
 
-            createItemInstance() {
-                if (this.selectedOption) {
-                    this.$router.push(`/contact-center/queues/${this.selectedOption}/new`);
-                }
-            },
+    createQueue() {
+      if (this.selected) {
+        this.$router.push(`/contact-center/queues/${this.selected.value}/new`);
+      }
+    },
 
-            computeSelectedOption(option) {
-                return option === this.selectedOption;
-            },
-        },
+    isSelected(option) {
+      return option === this.selected;
+    },
+  },
 
-    };
+};
 </script>
 
 <style lang="scss" scoped>
-    .popup-subheading {
-        @extend .typo-input-label;
-        margin: 0 0 28px;
+.create-queue-popup {
+  --min-popup-width: 600px;
+
+  ::v-deep .wt-popup__popup {
+    min-width: var(--min-popup-width);
+  }
+}
+
+.popup-options {
+  margin-top: 20px;
+  padding-right: 10px;
+
+  .popup-options__item-wrap {
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding: 7px 10px;
+    margin-bottom: 10px;
+    border: 1px solid var(--form-border-color);
+    border-radius: var(--border-radius);
+    cursor: pointer;
+
+    &:last-child {
+      margin-bottom: 0;
     }
 
-    .popup-options {
-        margin-bottom: 28px;
-
-        .popup-options__item-wrap {
-            position: relative;
-            padding: 28px 50px 28px 28px;
-            margin-bottom: 18px;
-            border: 2px solid $input;
-            border-radius: $border-radius;
-            cursor: pointer;
-
-            &:hover, &.active {
-                border-color: $info-color;
-            }
-        }
-
-        .popup-options__item-header {
-            @extend .typo-btn;
-            margin: 0 0 11px;
-            /*font-size: 1em;*/
-        }
-
-        .popup-options__item-text {
-            @extend .typo-body-md;
-            margin: 0;
-            /*font-size: 0.9em;*/
-        }
-
-        .popup-options__item-icon {
-            position: absolute;
-            top: 50%;
-            right: 26px;
-            transform: translateY(-50%);
-            color: $info-color;
-        }
+    &:hover, &.active {
+      border-color: var(--main-accent-color);
     }
+
+    .wt-icon-btn {
+      margin-left: auto;
+
+      ::v-deep .wt-tooltip {
+        width: calc(var(--min-popup-width) * 0.75);
+        top: 50%;
+        right: calc(100% + 10px);
+        left: auto;
+        transform: translate(0, -50%);
+      }
+    }
+  }
+
+  .popup-options__item-header {
+    @extend %typo-strong-md;
+  }
+}
 </style>
