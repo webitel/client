f8d967fe235341f9377d41b97ce90ed8f4ef8ac1
dlohvinov <nsblnr@gmail.com>
2021-02-01 12:33:08 +0200

    fix: fixed routing permissions check before userinfo loading [WTEL-1211]
    now user info is loaded before vue app init

--- src/components/object-utils/the-object-wrap.vue
+++ src/components/object-utils/the-object-wrap.vue
@@ -4,23 +4,18 @@
       <wt-notifications-bar/>
       <app-header></app-header>
       <div class="object-content-wrap">
-        <wt-loader v-if="isLoading"></wt-loader>
-        <router-view v-else></router-view>
+        <router-view></router-view>
       </div>
     </main>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
 import AppHeader from '../shared/app-header/app-header.vue';
 
 export default {
   name: 'module-wrap',
   components: { AppHeader },
-  computed: mapState('userinfo', {
-    isLoading: (state) => state.isLoading,
-  }),
 };
 </script>
 
@@ -33,12 +28,6 @@ export default {
 
 .object-content-wrap {
   flex-grow: 1;
-  display: flex;
   padding: 20px 30px;
-}
-
-.wt-loader {
-  align-self: center;
-  justify-self: center;
 }
 </style>
