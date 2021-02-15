d31fa35e0c9eeee935251d02502af9cc3a438187
dlohvinov <nsblnr@gmail.com>
2021-02-15 16:38:26 +0200

    refactor: refactored integrations/storage with edit AC [WTEL-1708]

--- src/components/integrations/storage/opened-storage.vue
+++ src/components/integrations/storage/opened-storage.vue
@@ -1,173 +1,174 @@
 <template>
-    <div class="content-wrap">
-        <object-header
-                :primaryText="computePrimaryText"
-                :primaryAction="save"
-                :primaryDisabled="computeDisabled"
-                close
-        >
-          <headline-nav :path="path"></headline-nav>
-        </object-header>
-        <tabs-component
-                :tabs="tabs"
-                :root="$options.name"
-        >
-            <template slot="component" slot-scope="props">
-                <component
-                        class="tabs-inner-component"
-                        :is="props.currentTab"
-                        :v="$v"
-                ></component>
-            </template>
-        </tabs-component>
-    </div>
+  <div class="content-wrap">
+    <object-header
+      :primary-text="computePrimaryText"
+      :primary-action="save"
+      :hide-primary="!hasSaveActionAccess"
+      :primary-disabled="computeDisabled"
+      :secondary-action="close"
+    >
+      <headline-nav :path="path"></headline-nav>
+    </object-header>
+    <tabs-component
+      :tabs="tabs"
+      :root="$options.name"
+    >
+      <template slot="component" slot-scope="props">
+        <component
+          class="tabs-inner-component"
+          :is="props.currentTab"
+          :v="$v"
+        ></component>
+      </template>
+    </tabs-component>
+  </div>
 </template>
 
 <script>
-    import editComponentMixin from '@/mixins/objectPagesMixins/openedObjectMixin/editComponentMixin';
-    import { required, email } from 'vuelidate/lib/validators';
-    import { mapActions, mapState } from 'vuex';
-    import openedStorageGeneral from './opened-storage-general';
-    import openedStorageLocal from './opened-storage-local';
-    import openedStorageS3 from './opened-storage-s3';
-    import openedStorageBackblaze from './opened-storage-backblaze';
-    import openedStorageDropbox from './opened-storage-dropbox';
-    import openedStorageDrive from './opened-storage-drive';
-    import headlineNavMixin from '../../../mixins/baseMixins/headlineNavMixin/headlineNavMixin';
+import editComponentMixin from '@/mixins/objectPagesMixins/openedObjectMixin/editComponentMixin';
+import { required } from 'vuelidate/lib/validators';
+import { mapActions, mapState } from 'vuex';
+import openedStorageGeneral from './opened-storage-general';
+import openedStorageLocal from './opened-storage-local';
+import openedStorageS3 from './opened-storage-s3';
+import openedStorageBackblaze from './opened-storage-backblaze';
+import openedStorageDropbox from './opened-storage-dropbox';
+import openedStorageDrive from './opened-storage-drive';
+import headlineNavMixin from '../../../mixins/baseMixins/headlineNavMixin/headlineNavMixin';
 
-    export default {
-        name: 'opened-storage',
-        mixins: [editComponentMixin, headlineNavMixin],
-        components: {
-            openedStorageGeneral,
-            openedStorageLocal,
-            openedStorageS3,
-            openedStorageBackblaze,
-            openedStorageDropbox,
-            openedStorageDrive,
-        },
-        data() {
-            return {};
-        },
+export default {
+  name: 'opened-storage',
+  mixins: [editComponentMixin, headlineNavMixin],
+  components: {
+    openedStorageGeneral,
+    openedStorageLocal,
+    openedStorageS3,
+    openedStorageBackblaze,
+    openedStorageDropbox,
+    openedStorageDrive,
+  },
+  data() {
+    return {};
+  },
 
-        validations() {
-            switch (this.$route.params.type) {
-                case 'local':
-                    return {
-                        itemInstance: {
-                            name: { required },
-                        },
-                    };
-                case 's3':
-                    return {
-                        itemInstance: {
-                            name: { required },
-                            properties: {
-                                keyId: { required },
-                                accessKey: { required },
-                                bucketName: { required },
-                                region: { required },
-                                endpoint: { required },
-                            },
-                        },
-                    };
-                case 'backblaze':
-                    return {
-                        itemInstance: {
-                            name: { required },
-                            account: { required },
-                            key: { required },
-                            bucket: { required },
-                            bucketId: { required },
-                        },
-                    };
-                case 'dropbox':
-                    return {
-                        itemInstance: {
-                            name: { required },
-                            properties: {
-                                token: { required },
-                            },
-                        },
-                    };
-                case 'drive':
-                    return {
-                        itemInstance: {
-                            name: { required },
-                            properties: {
-                                directory: { required },
-                                privateKey: { required },
-                                email: { required },
-                            },
-                        },
-                    };
-            }
-        },
-
-        mounted() {
-            this.id = this.$route.params.id;
-            this.loadItem(this.$route.params.type);
-        },
-
-        computed: {
-            ...mapState('integrations/storage', {
-                itemInstance: (state) => state.itemInstance,
-            }),
-            id: {
-                get() {
-                    return this.$store.state.integrations.storage.itemId;
-                },
-                set(value) {
-                    this.setId(value);
-                },
-            },
-            tabs() {
-                const tabs = [{ text: this.$t('objects.general'), value: 'general' }];
-                switch (this.$route.params.type) {
-                    case 'local':
-                        tabs.push({ text: this.$t('objects.integrations.storage.configuration'), value: 'local' });
-                        break;
-                    case 's3':
-                        tabs.push({ text: this.$t('objects.integrations.storage.configuration'), value: 's3' });
-                        break;
-                    case 'backblaze':
-                        tabs.push({ text: this.$t('objects.integrations.storage.configuration'), value: 'backblaze' });
-                        break;
-                    case 'dropbox':
-                        tabs.push({ text: this.$t('objects.integrations.storage.configuration'), value: 'dropbox' });
-                        break;
-                    case 'drive':
-                        tabs.push({ text: this.$t('objects.integrations.storage.configuration'), value: 'drive' });
-                        break;
-                }
-                return tabs;
-            },
-
-          path() {
-              const { type } = this.$route.params;
-              const baseUrl = '/integrations/storage';
-              const url = `${baseUrl}/${type}`;
-            return [
-              { name: this.$t('objects.integrations.integrations') },
-              { name: this.$t('objects.integrations.storage.storage'), route: baseUrl },
-              {
-                name: this.id ? this.pathName : this.$t('objects.new'),
-                route: this.id ? `${url}/${this.id}` : `${url}/new`,
-              },
-            ];
+  validations() {
+    switch (this.$route.params.type) {
+      case 'local':
+        return {
+          itemInstance: {
+            name: { required },
           },
-        },
+        };
+      case 's3':
+        return {
+          itemInstance: {
+            name: { required },
+            properties: {
+              keyId: { required },
+              accessKey: { required },
+              bucketName: { required },
+              region: { required },
+              endpoint: { required },
+            },
+          },
+        };
+      case 'backblaze':
+        return {
+          itemInstance: {
+            name: { required },
+            account: { required },
+            key: { required },
+            bucket: { required },
+            bucketId: { required },
+          },
+        };
+      case 'dropbox':
+        return {
+          itemInstance: {
+            name: { required },
+            properties: {
+              token: { required },
+            },
+          },
+        };
+      case 'drive':
+        return {
+          itemInstance: {
+            name: { required },
+            properties: {
+              directory: { required },
+              privateKey: { required },
+              email: { required },
+            },
+          },
+        };
+    }
+  },
 
-        methods: {
-            ...mapActions('integrations/storage', {
-                setId: 'SET_ITEM_ID',
-                loadItem: 'LOAD_ITEM',
-                addItem: 'ADD_ITEM',
-                updateItem: 'UPDATE_ITEM',
-                resetState: 'RESET_ITEM_STATE',
-            }),
+  mounted() {
+    this.id = this.$route.params.id;
+    this.loadItem(this.$route.params.type);
+  },
+
+  computed: {
+    ...mapState('integrations/storage', {
+      itemInstance: (state) => state.itemInstance,
+    }),
+    id: {
+      get() {
+        return this.$store.state.integrations.storage.itemId;
+      },
+      set(value) {
+        this.setId(value);
+      },
+    },
+    tabs() {
+      const tabs = [{ text: this.$t('objects.general'), value: 'general' }];
+      switch (this.$route.params.type) {
+        case 'local':
+          tabs.push({ text: this.$t('objects.integrations.storage.configuration'), value: 'local' });
+          break;
+        case 's3':
+          tabs.push({ text: this.$t('objects.integrations.storage.configuration'), value: 's3' });
+          break;
+        case 'backblaze':
+          tabs.push({ text: this.$t('objects.integrations.storage.configuration'), value: 'backblaze' });
+          break;
+        case 'dropbox':
+          tabs.push({ text: this.$t('objects.integrations.storage.configuration'), value: 'dropbox' });
+          break;
+        case 'drive':
+          tabs.push({ text: this.$t('objects.integrations.storage.configuration'), value: 'drive' });
+          break;
+      }
+      return tabs;
+    },
+
+    path() {
+      const { type } = this.$route.params;
+      const baseUrl = '/integrations/storage';
+      const url = `${baseUrl}/${type}`;
+      return [
+        { name: this.$t('objects.integrations.integrations') },
+        { name: this.$t('objects.integrations.storage.storage'), route: baseUrl },
+        {
+          name: this.id ? this.pathName : this.$t('objects.new'),
+          route: this.id ? `${url}/${this.id}` : `${url}/new`,
         },
-    };
+      ];
+    },
+  },
+
+  methods: {
+    ...mapActions('integrations/storage', {
+      setId: 'SET_ITEM_ID',
+      loadItem: 'LOAD_ITEM',
+      addItem: 'ADD_ITEM',
+      updateItem: 'UPDATE_ITEM',
+      resetState: 'RESET_ITEM_STATE',
+    }),
+  },
+};
 </script>
 
 <style scoped>
--- src/components/integrations/storage/the-storage.vue
+++ src/components/integrations/storage/the-storage.vue
@@ -1,200 +1,206 @@
 <template>
-    <div class="content-wrap">
-      <object-header
-        :hide-primary="!isCreateAccess"
-        :primary-action="create"
+  <div class="content-wrap">
+    <object-header
+      :hide-primary="!isCreateAccess"
+      :primary-action="create"
+    >
+      <headline-nav :path="path"></headline-nav>
+    </object-header>
+
+    <storage-popup
+      v-if="popupTriggerIf"
+      @close="popupTriggerIf = false"
+    ></storage-popup>
+
+    <section class="object-content">
+      <header class="content-header">
+        <h3 class="content-title">{{ $t('objects.integrations.storage.allStorages') }}</h3>
+        <div class="content-header__actions-wrap">
+          <search
+            v-model="search"
+            @filterData="loadList"
+          ></search>
+          <i
+            v-if="isDeleteAccess"
+            class="icon-icon_delete icon-action"
+            :class="{'hidden': anySelected}"
+            :title="$t('iconHints.deleteSelected')"
+            @click="deleteSelected"
+          ></i>
+          <i
+            class="icon-icon_reload icon-action"
+            :title="$t('iconHints.reload')"
+            @click="loadList"
+          ></i>
+        </div>
+      </header>
+
+      <loader v-show="!isLoaded"></loader>
+
+      <vuetable
+        v-show="isLoaded"
+        :api-mode="false"
+        :fields="fields"
+        :data="dataList"
       >
-          <headline-nav :path="path"></headline-nav>
-        </object-header>
+        <template slot="name" slot-scope="props">
+          <div>
+            <span class="nameLink" @click="edit(props.rowIndex)">
+              {{ dataList[props.rowIndex].name }}
+            </span>
+          </div>
+        </template>
 
-        <storage-popup
-            v-if="popupTriggerIf"
-            @close="popupTriggerIf = false"
-        ></storage-popup>
+        <template slot="type" slot-scope="props">
+          <div>
+            {{ computeType(dataList[props.rowIndex].type) }}
+          </div>
+        </template>
 
-        <section class="object-content">
-            <header class="content-header">
-                <h3 class="content-title">{{$t('objects.integrations.storage.allStorages')}}</h3>
-                <div class="content-header__actions-wrap">
-                    <search
-                            v-model="search"
-                            @filterData="loadList"
-                    ></search>
-                    <i
-                            v-if="isDeleteAccess"
-                            class="icon-icon_delete icon-action"
-                            :class="{'hidden': anySelected}"
-                            :title="$t('iconHints.deleteSelected')"
-                            @click="deleteSelected"
-                    ></i>
-                    <i
-                            class="icon-icon_reload icon-action"
-                            :title="$t('iconHints.reload')"
-                            @click="loadList"
-                    ></i>
-                </div>
-            </header>
+        <template slot="maxSize" slot-scope="props">
+          <div>
+            {{ dataList[props.rowIndex].maxSize }}
+          </div>
+        </template>
 
-            <loader v-show="!isLoaded"></loader>
+        <template slot="expireDays" slot-scope="props">
+          <div>
+            {{ dataList[props.rowIndex].expireDays }}
+          </div>
+        </template>
 
-            <vuetable
-                    v-show="isLoaded"
-                    :api-mode="false"
-                    :fields="fields"
-                    :data="dataList"
-            >
-                <template slot="name" slot-scope="props">
-                    <div>
-                        <span class="nameLink" @click="edit(props.rowIndex)">
-                        {{dataList[props.rowIndex].name}}
-                        </span>
-                    </div>
-                </template>
+        <template slot="enabled" slot-scope="props">
+          <switcher
+            :value="!dataList[props.rowIndex].disabled"
+            :disabled="!isEditAccess"
+            @input="patchProperty({index: props.rowIndex, prop: 'disabled', value: !$event})"
+          ></switcher>
+        </template>
 
-                <template slot="type" slot-scope="props">
-                    <div>
-                        {{computeType(dataList[props.rowIndex].type)}}
-                    </div>
-                </template>
-
-                <template slot="maxSize" slot-scope="props">
-                    <div>
-                        {{dataList[props.rowIndex].maxSize}}
-                    </div>
-                </template>
-
-                <template slot="expireDays" slot-scope="props">
-                    <div>
-                        {{dataList[props.rowIndex].expireDays}}
-                    </div>
-                </template>
-
-                <template slot="enabled" slot-scope="props">
-                    <switcher
-                            :value="!dataList[props.rowIndex].disabled"
-                            @input="patchProperty({index: props.rowIndex, prop: 'disabled', value: !$event})"
-                    ></switcher>
-                </template>
-
-                <template slot="actions" slot-scope="props">
-                    <i class="vuetable-action icon-icon_edit"
-                       v-if="isEditAccess"
-                       :title="$t('iconHints.edit')"
-                       @click="edit(props.rowIndex)"
-                    ></i>
-                    <i class="vuetable-action icon-icon_delete"
-                       v-if="isDeleteAccess"
-                       :title="$t('iconHints.delete')"
-                       @click="remove(props.rowIndex)"
-                    ></i>
-                </template>
-            </vuetable>
-            <pagination
-                    v-show="isLoaded"
-                    v-model="size"
-                    @loadDataList="loadList"
-                    @next="nextPage"
-                    @prev="prevPage"
-                    :isNext="isNextPage"
-                    :isPrev="!!page"
-                    :page="page"
-            ></pagination>
-        </section>
-    </div>
+        <template slot="actions" slot-scope="props">
+          <i class="vuetable-action icon-icon_edit"
+             v-if="isEditAccess"
+             :title="$t('iconHints.edit')"
+             @click="edit(props.rowIndex)"
+          ></i>
+          <i class="vuetable-action icon-icon_delete"
+             v-if="isDeleteAccess"
+             :title="$t('iconHints.delete')"
+             @click="remove(props.rowIndex)"
+          ></i>
+        </template>
+      </vuetable>
+      <pagination
+        v-show="isLoaded"
+        v-model="size"
+        @loadDataList="loadList"
+        @next="nextPage"
+        @prev="prevPage"
+        :isNext="isNextPage"
+        :isPrev="!!page"
+        :page="page"
+      ></pagination>
+    </section>
+  </div>
 </template>
 
 <script>
-    import tableComponentMixin from '@/mixins/objectPagesMixins/objectTableMixin/tableComponentMixin';
-    import { mapActions, mapState } from 'vuex';
-    import storagePopup from './create-storage-popup';
-    import { _checkboxTableField, _actionsTableField_2, _switcherWidth } from '../../../utils/tableFieldPresets';
-    import RouteNames from '../../../router/_internals/RouteNames.enum';
+import tableComponentMixin from '@/mixins/objectPagesMixins/objectTableMixin/tableComponentMixin';
+import { mapActions, mapState } from 'vuex';
+import storagePopup from './create-storage-popup';
+import { _actionsTableField_2, _checkboxTableField, _switcherWidth } from '../../../utils/tableFieldPresets';
+import RouteNames from '../../../router/_internals/RouteNames.enum';
 
-    export default {
-        name: 'the-storage',
-        mixins: [tableComponentMixin],
-        components: { storagePopup },
-        computed: {
-            ...mapState('integrations/storage', {
-                dataList: (state) => state.dataList,
-                page: (state) => state.page,
-                isNextPage: (state) => state.isNextPage,
-            }),
+export default {
+  name: 'the-storage',
+  mixins: [tableComponentMixin],
+  components: { storagePopup },
+  computed: {
+    ...mapState('integrations/storage', {
+      dataList: (state) => state.dataList,
+      page: (state) => state.page,
+      isNextPage: (state) => state.isNextPage,
+    }),
 
-            size: {
-                get() {
-                    return this.$store.state.integrations.storage.size;
-                },
-                set(value) {
-                    this.setSize(value);
-                },
-            },
+    size: {
+      get() {
+        return this.$store.state.integrations.storage.size;
+      },
+      set(value) {
+        this.setSize(value);
+      },
+    },
 
-            search: {
-                get() {
-                    return this.$store.state.integrations.storage.search;
-                },
-                set(value) {
-                    this.setSearch(value);
-                },
-            },
+    search: {
+      get() {
+        return this.$store.state.integrations.storage.search;
+      },
+      set(value) {
+        this.setSearch(value);
+      },
+    },
 
-          fields() {
-            let fields = [
-              _checkboxTableField,
-              { name: 'name', title: this.$t('objects.name') },
-              { name: 'type', title: this.$t('objects.integrations.storage.type') },
-              { name: 'maxSize', title: this.$t('objects.integrations.storage.maxSize') },
-              { name: 'expireDays', title: this.$t('objects.integrations.storage.expireDays') },
-              { name: 'enabled', title: this.$t('objects.enabled'), width: _switcherWidth },
-            ];
-            if (this.hasTableActions) fields = fields.concat(_actionsTableField_2);
-            return fields;
-          },
-          path() {
-            return [
-              { name: this.$t('objects.integrations.integrations') },
-              { name: this.$t('objects.integrations.storage.storage'), route: '/integrations/storage' },
-            ];
-          },
+    fields() {
+      let fields = [
+        _checkboxTableField,
+        { name: 'name', title: this.$t('objects.name') },
+        { name: 'type', title: this.$t('objects.integrations.storage.type') },
+        { name: 'maxSize', title: this.$t('objects.integrations.storage.maxSize') },
+        { name: 'expireDays', title: this.$t('objects.integrations.storage.expireDays') },
+        { name: 'enabled', title: this.$t('objects.enabled'), width: _switcherWidth },
+      ];
+      if (this.hasTableActions) fields = fields.concat(_actionsTableField_2);
+      return fields;
+    },
+    path() {
+      return [
+        { name: this.$t('objects.integrations.integrations') },
+        { name: this.$t('objects.integrations.storage.storage'), route: '/integrations/storage' },
+      ];
+    },
+  },
+
+  methods: {
+    create() {
+      this.popupTriggerIf = true;
+    },
+
+    edit(rowId) {
+      this.$router.push({
+        name: `${RouteNames.STORAGE}-edit`,
+        params: {
+          type: this.dataList[rowId].type,
+          id: this.dataList[rowId].id,
         },
+      });
+    },
 
-        methods: {
-            create() {
-                this.popupTriggerIf = true;
-            },
+    computeType(type) {
+      switch (type) {
+        case 'local':
+          return 'Local';
+        case 's3':
+          return 'S3 Bucket';
+        case 'backblaze':
+          return 'Backblaze';
+        case 'dropbox':
+          return 'Dropbox';
+        case 'drive':
+          return 'Google Drive';
+      }
+    },
 
-            edit(rowId) {
-                this.$router.push({
-                    name: `${RouteNames.STORAGE}-edit`,
-                    params: {
-                        type: this.dataList[rowId].type,
-                        id: this.dataList[rowId].id,
-                    },
-                });
-            },
-
-            computeType(type) {
-                switch (type) {
-                    case 'local': return 'Local';
-                    case 's3': return 'S3 Bucket';
-                    case 'backblaze': return 'Backblaze';
-                    case 'dropbox': return 'Dropbox';
-                    case 'drive': return 'Google Drive';
-                }
-            },
-
-            ...mapActions('integrations/storage', {
-                loadDataList: 'LOAD_DATA_LIST',
-                patchProperty: 'PATCH_ITEM_PROPERTY',
-                setSize: 'SET_SIZE',
-                setSearch: 'SET_SEARCH',
-                nextPage: 'NEXT_PAGE',
-                prevPage: 'PREV_PAGE',
-                removeItem: 'REMOVE_ITEM',
-            }),
-        },
-    };
+    ...mapActions('integrations/storage', {
+      loadDataList: 'LOAD_DATA_LIST',
+      patchProperty: 'PATCH_ITEM_PROPERTY',
+      setSize: 'SET_SIZE',
+      setSearch: 'SET_SEARCH',
+      nextPage: 'NEXT_PAGE',
+      prevPage: 'PREV_PAGE',
+      removeItem: 'REMOVE_ITEM',
+    }),
+  },
+};
 </script>
 
 <style lang="scss" scoped>
