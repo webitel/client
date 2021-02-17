2041709586c69b2e2bd86667a33b8bcf31ba1396
dlohvinov <nsblnr@gmail.com>
2021-02-17 16:58:36 +0200

    refactor: added userResponseHandler to Api List Getter [WTEL-1744]

--- src/api/utils/ApiControllers/ListGetter/SDKListGetter.js
+++ src/api/utils/ApiControllers/ListGetter/SDKListGetter.js
@@ -4,8 +4,10 @@ import BaseListGetter from './BaseListGetter';
 export class WebitelSDKListGetter extends BaseListGetter {
   async _getList(args) {
     try {
-      const response = await this.method(...args);
-      return this.responseHandler(response);
+      let response = await this.method(...args);
+      response = this.responseHandler(response);
+      if (this.userResponseHandler) response = this.userResponseHandler(response);
+      return response;
     } catch (err) {
       throw err;
     }
