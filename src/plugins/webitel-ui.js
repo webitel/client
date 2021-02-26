888d03044d51363011a2514e6414b9598f1f0551
dlohvinov <nsblnr@gmail.com>
2021-02-26 16:31:47 +0200

    feature: added ua locale, merged webitel-ui ua locale file tempalte [WTEL-1772]

--- src/plugins/webitel-ui.js
+++ src/plugins/webitel-ui.js
@@ -5,6 +5,7 @@ import WebitelUI from '@webitel/ui-sdk/dist/ui-sdk.common';
 // import locale
 import WebitelUIEn from '@webitel/ui-sdk/src/locale/en/en';
 import WebitelUIRu from '@webitel/ui-sdk/src/locale/ru/ru';
+import WebitelUIUa from '@webitel/ui-sdk/src/locale/ua/ua';
 import eventBus from '@webitel/ui-sdk/src/scripts/eventBus';
 import i18n from '../locale/i18n';
 
@@ -19,3 +20,4 @@ Vue.use(WebitelUI, { eventBus });
 // add plugin locales to main i18n
 i18n.mergeLocaleMessage('en', WebitelUIEn);
 i18n.mergeLocaleMessage('ru', WebitelUIRu);
+i18n.mergeLocaleMessage('ua', WebitelUIUa);
