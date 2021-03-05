2733db4c61d0876404ef6061252401b5690d2aff
dlohvinov <nsblnr@gmail.com>
2021-03-05 12:41:34 +0200

    feature: refactored editor, added expand/collapse functionality [WTEL-1790]

--- src/components/routing/flow/opened-flow-general.vue
+++ src/components/routing/flow/opened-flow-general.vue
@@ -3,7 +3,7 @@
     <header class="content-header">
       <h3 class="content-title">{{ $t('objects.generalInfo') }}</h3>
     </header>
-    <form class="object-input-grid object-input-grid__1-col">
+    <form class="object-input-grid object-input-grid__1-col object-input-grid__w50">
       <wt-input
         :value="itemInstance.name"
         :v="v.itemInstance.name"
@@ -12,14 +12,14 @@
         required
         @input="setItemProp({ prop: 'name', value: $event })"
       ></wt-input>
-      <code-editor
+    </form>
+    <code-editor
         :value="itemInstance.schema"
         :label="$t('objects.routing.flow.callFlow')"
         :disabled="disableUserInput"
         @errorListener="isSyntaxError = $event"
         @change="setItemProp({ prop: 'schema', value: $event })"
       ></code-editor>
-    </form>
   </section>
 </template>
 
