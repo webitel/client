beba419cc5264872e54047f0f66bc0d21df7b3e4
navrotskyj <navrotskyj@gmail.com>
2021-05-25 12:34:33 +0300

    fixed queue parameters

--- src/modules/contact-center/modules/queues/components/outbound-ivr-queue/opened-queue-outbound-ivr-timing.vue
+++ src/modules/contact-center/modules/queues/components/outbound-ivr-queue/opened-queue-outbound-ivr-timing.vue
@@ -8,26 +8,26 @@
         :value="itemInstance.payload.originateTimeout"
         :label="$t('objects.ccenter.queues.originateTimeout')"
         :disabled="disableUserInput"
-        @input="setItemPayloadProp({ prop: 'originateTimeout', value: $event })"
+        @input="setItemPayloadProp({ prop: 'originateTimeout', value: +$event })"
       ></wt-timepicker>
       <wt-input
         :value="itemInstance.payload.maxAttempts"
         :label="$t('objects.ccenter.queues.maxAttempts')"
         :disabled="disableUserInput"
         type="number"
-        @input="setItemPayloadProp({ prop: 'maxAttempts', value: $event })"
+        @input="setItemPayloadProp({ prop: 'maxAttempts', value: +$event })"
       ></wt-input>
       <wt-timepicker
         :value="itemInstance.payload.waitBetweenRetries"
         :label="$t('objects.ccenter.queues.waitBetweenRetries')"
         :disabled="disableUserInput"
-        @input="setItemPayloadProp({ prop: 'waitBetweenRetries', value: $event })"
+        @input="setItemPayloadProp({ prop: 'waitBetweenRetries', value: +$event })"
       ></wt-timepicker>
       <wt-timepicker
         :value="itemInstance.payload.minDuration"
         :label="$t('objects.ccenter.queues.minDuration')"
         :disabled="disableUserInput"
-        @input="setItemPayloadProp({ prop: 'minDuration', value: $event })"
+        @input="setItemPayloadProp({ prop: 'minDuration', value: +$event })"
       ></wt-timepicker>
       <wt-switcher
         :value="itemInstance.payload.recordings"
