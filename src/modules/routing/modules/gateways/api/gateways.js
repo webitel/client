9c1a8db17786ceb260ce144fd69286a2ffcd529c
vasjamel <vasjanja@gmail.com>
2021-07-14 12:16:02 +0300

    fix: gateway description fixed and sip validation added

--- src/modules/routing/modules/gateways/api/gateways.js
+++ src/modules/routing/modules/gateways/api/gateways.js
@@ -12,7 +12,7 @@ import trunkingGateway
 
 const BASE_URL = '/sip/gateways';
 const fieldsToSend = ['name', 'proxy', 'id', 'host', 'ipacl', 'account', 'username', 'expires',
-  'account', 'registrar', 'register', 'password', 'schema', 'description', 'enable'];
+  'account', 'registrar', 'name', 'register', 'password', 'schema', 'usage', 'enable'];
 
 const defaultListObject = { // default object prototype, to merge response with it to get all fields
   _isSelected: false,
--- src/modules/routing/modules/gateways/store/_internals/gatewaySchema/defaults/defaultGateway.js
+++ src/modules/routing/modules/gateways/store/_internals/gatewaySchema/defaults/defaultGateway.js
@@ -1,6 +1,6 @@
 const defaultGateway = () => ({
   name: '',
-  description: '',
+  usage: '',
   proxy: '',
   schema: {},
   enable: true,
