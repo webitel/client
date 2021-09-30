4d65bc871b527ce8d02e7be6881a2487d7129828
Vasjamel <vasjanja@gmail.com>
2021-09-30 15:31:45 +0300

    refactor hslToRgba converter [WTEL-1992]

--- src/modules/routing/modules/chat-gateways/mixins/webChatViewFormMixin.js
+++ src/modules/routing/modules/chat-gateways/mixins/webChatViewFormMixin.js
@@ -1,3 +1,15 @@
+const convertHslToRgb = ({ h, s, l }) => {
+  const k = (n) => (n + h / 30) % 12;
+  const a = s * Math.min(l, 1 - l);
+  const f = (n) => l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+  const rgbColor = {
+    r: Math.round(255 * f(0)),
+    g: Math.round(255 * f(8)),
+    b: Math.round(255 * f(4)),
+  };
+  return rgbColor;
+};
+
 export default {
   data: () => ({
     isCopied: false,
@@ -6,8 +18,8 @@ export default {
     selectedLanguage: {},
     color: {
       a: 1,
-      hsl: { h: 0, s: 100, l: 50, a: 1 },
-      rgba: { r: 0, g: 0, b: 0, a: 1 },
+      hsl: { h: 42, s: 1, l: 0.5, a: 1 },
+      rgba: { r: 255, g: 179, b: 0, a: 1 },
     },
   }),
   computed: {
@@ -39,10 +51,13 @@ export default {
       }, {
         name: this.$t('objects.routing.chatGateways.metadata.left'),
         value: 'left',
-      }, {
-        name: this.$t('objects.routing.chatGateways.metadata.static'),
-        value: 'static',
-      }];
+      },
+      // Uncomment position static if needed. Not available until documentation is ready.
+      // {
+      // name: this.$t('objects.routing.chatGateways.metadata.static'),
+      // value: 'static',
+      // }
+      ];
     },
     buttonLabel() {
       return this.isCopied ? this.$t('objects.copied') : this.$t('objects.routing.chatGateways.metadata.copy');
@@ -74,21 +89,11 @@ export default {
       const l = +this.color.hsl.l.toFixed(2) * 100;
       const hsl = `hsl(${h}, ${s}%, ${l}%)`;
       this.setItemMetadata({ prop: 'accentColor', value: hsl });
-      this.setItemMetadata({ prop: 'btnOpacity', value: `${this.color.a}` });
     },
     setAlpha(value) {
-      this.color = {
-        ...this.color,
-        rgba: {
-          ...this.color.rgba,
-          a: value.a,
-        },
-        hsl: {
-          ...this.color.hsl,
-          a: value.a,
-        },
-        a: value.a,
-      };
+      this.color.hsl.a = value.a;
+      this.color.rgba.a = value.a;
+      this.color.a = value.a;
       this.setItemMetadata({ prop: 'btnOpacity', value: `${value.a}` });
     },
     restoreOpacity(value) {
@@ -107,29 +112,15 @@ export default {
           s: +colorArray[1] / 100,
           l: +colorArray[2] / 100,
         };
-        this.convertHslToRgb(+colorArray[0], +colorArray[1], +colorArray[2]);
-        this.restoreOpacity(this.itemInstance.metadata.btnOpacity);
+        this.color.rgba = convertHslToRgb(this.color.hsl);
       }
     },
     restoreConfig() {
       this.restoreLanguage(this.itemInstance.metadata.lang);
       this.restorePosition(this.itemInstance.metadata.position);
       this.restoreBorderRadius(this.itemInstance.metadata.borderRadiusStyle);
-      this.restoreOpacity(this.itemInstance.metadata.btnOpacity);
       this.restoreColor(this.itemInstance.metadata.accentColor);
-    },
-    convertHslToRgb(h, s, l) {
-      s /= 100;
-      l /= 100;
-      const k = n => (n + h / 30) % 12;
-      const a = s * Math.min(l, 1 - l);
-      const f = n =>
-        l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
-      this.color.rgba = {
-        r: Math.round(255 * f(0)),
-        g: Math.round(255 * f(8)),
-        b: Math.round(255 * f(4)),
-      };
+      this.restoreOpacity(this.itemInstance.metadata.btnOpacity);
     },
   },
   created() {
