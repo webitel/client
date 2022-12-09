fa13dabcb51411f78b3b4482001b238d2a50fda3
dlohvinov <nsblnr@gmail.com>
2022-12-09 17:53:59 +0200

    fix: fixed appointments code generation [WTEL-3019]

--- src/modules/routing/modules/chat-gateways/modules/webchat/components/copy-code-button.vue
+++ src/modules/routing/modules/chat-gateways/modules/webchat/components/copy-code-button.vue
@@ -51,7 +51,7 @@ const processAppointmentConfig = ({
                                   }, uri) => {
   if (!enabled) return undefined;
   const result = { ...filterEmptyValues(rest) };
-  result.url = new URL(path.join(CHAT_URL.replace('chat', 'appointment'), uri), SCRIPT_URL);
+  result.url = new URL(path.join(CHAT_URL.replace('chat', 'appointments'), uri), SCRIPT_URL);
   return result;
 };
 
