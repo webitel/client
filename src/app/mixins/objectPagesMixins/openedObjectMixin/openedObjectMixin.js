fe85a6a40196a89868e4becd133247248c5c7dee
volodymyr <volodymyr.dechtiaruk@gmail.com>
2023-06-07 10:56:17 +0300

    fix: validation for regExp added WTEL-3400 (trying to fix line separators in openedObjectMixin)

--- src/app/mixins/objectPagesMixins/openedObjectMixin/openedObjectMixin.js
+++ src/app/mixins/objectPagesMixins/openedObjectMixin/openedObjectMixin.js
@@ -1,1 +1,74 @@
-import { mapActions, mapState } from 'vuex';import getNamespacedState from '@webitel/ui-sdk/src/store/helpers/getNamespacedState';import ObjectHeader from '../../../components/utils/object-utils/the-object-header.vue';import Permissions from '../../../../modules/_shared/permissions-tab/components/permissions-tab.vue';import baseObjectMixin from '../../baseMixins/baseObjectMixin/baseObjectMixin';import headlineNavMixin from '../../baseMixins/headlineNavMixin/headlineNavMixin';import openedObjectAccessControlMixin from './_internals/openedObjectAccessControlMixin';/** * @fileOverview contains openedObject (wrapper with tabs, like opened-agent.vue) common logic * @param {string} this.namespace - should be declared in data() *      and contain a string name for storeModule like 'ccenter/agents/skills' * @extends baseObjectMixin */export default {  mixins: [    openedObjectAccessControlMixin,    headlineNavMixin,    baseObjectMixin,  ],  components: {    ObjectHeader,    Permissions,  },  data: () => ({    currentTab: {      value: 'general',    },  }),  created() {    this.setInitialTab();    this.loadPageData();  },  computed: {    ...mapState({      id(state) {        return getNamespacedState(state, this.namespace).itemId;      },      itemInstance(state) {        return getNamespacedState(state, this.namespace).itemInstance;      },    }),    permissionsTab() {      return {        text: this.$tc('objects.permissions.permissions', 2),        value: 'permissions',      };    },  },  methods: {    ...mapActions({      setId(dispatch, payload) {        return dispatch(`${this.namespace}/SET_ITEM_ID`, payload);      },    }),    async loadPageData() {      await this.setId(this.$route.params.id);      return this.loadItem();    },    setInitialTab() {      // eslint-disable-next-line prefer-destructuring      if (this.tabs) this.currentTab = this.tabs[0];    },    close() {      this.$router.go(-1);    },  },};
+import { mapActions, mapState } from 'vuex';
+import getNamespacedState from '@webitel/ui-sdk/src/store/helpers/getNamespacedState';
+import ObjectHeader from '../../../components/utils/object-utils/the-object-header.vue';
+import Permissions from '../../../../modules/_shared/permissions-tab/components/permissions-tab.vue';
+import baseObjectMixin from '../../baseMixins/baseObjectMixin/baseObjectMixin';
+import headlineNavMixin from '../../baseMixins/headlineNavMixin/headlineNavMixin';
+import openedObjectAccessControlMixin from './_internals/openedObjectAccessControlMixin';
+
+/**
+ * @fileOverview contains openedObject (wrapper with tabs, like opened-agent.vue) common logic
+ * @param {string} this.namespace - should be declared in data()
+ *      and contain a string name for storeModule like 'ccenter/agents/skills'
+ * @extends baseObjectMixin
+ */
+export default {
+  mixins: [
+    openedObjectAccessControlMixin,
+    headlineNavMixin,
+    baseObjectMixin,
+  ],
+  components: {
+    ObjectHeader,
+    Permissions,
+  },
+  data: () => ({
+    currentTab: {
+      value: 'general',
+    },
+  }),
+
+  created() {
+    this.setInitialTab();
+    this.loadPageData();
+  },
+
+  computed: {
+    ...mapState({
+      id(state) {
+        return getNamespacedState(state, this.namespace).itemId;
+      },
+      itemInstance(state) {
+        return getNamespacedState(state, this.namespace).itemInstance;
+      },
+    }),
+    permissionsTab() {
+      return {
+        text: this.$tc('objects.permissions.permissions', 2),
+        value: 'permissions',
+      };
+    },
+  },
+
+  methods: {
+    ...mapActions({
+      setId(dispatch, payload) {
+        return dispatch(`${this.namespace}/SET_ITEM_ID`, payload);
+      },
+    }),
+
+    async loadPageData() {
+      await this.setId(this.$route.params.id);
+      return this.loadItem();
+    },
+
+    setInitialTab() {
+      // eslint-disable-next-line prefer-destructuring
+      if (this.tabs) this.currentTab = this.tabs[0];
+    },
+
+    close() {
+      this.$router.go(-1);
+    },
+  },
+};
