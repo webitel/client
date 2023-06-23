ebee21094ec8887df5c6107f668b5fc7041fa7da
Lera24 <bedulinavalera21@gmail.com>
2023-06-23 15:34:52 +0300

    fix: unit test[WTEL-3357/3512]

--- src/modules/integrations/modules/email-profiles/components/__tests__/the-email-profiles.spec.js
+++ src/modules/integrations/modules/email-profiles/components/__tests__/the-email-profiles.spec.js
@@ -1,7 +1,12 @@
+import { ref } from 'vue';
 import { shallowMount } from '@vue/test-utils';
 import TheEmailProfiles from '../the-email-profiles.vue';
 import tableComponentComputedMock
   from '../../../../../../../tests/unit/mocks/tableComponentComputed.mock';
+import { useDummy } from '../../../../../../app/composables/useDummy';
+
+jest.mock('../../../../../../app/composables/useDummy');
+useDummy.mockImplementation(() => ({ reloadSortable: ref(false) }));
 
 describe('TheEmailProfiles', () => {
     it('renders a component', () => {
