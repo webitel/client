b004b3d73196042225256e2ac21a5548c72c4e87
lizacoma <my.skin00@gmail.com>
2023-07-11 21:21:05 +0300

    feature: roles api refactoring [WTEL-3630]

--- src/modules/permissions/modules/roles/api/roles.js
+++ src/modules/permissions/modules/roles/api/roles.js
@@ -10,15 +10,17 @@ import applyTransform, {
 } from '@webitel/ui-sdk/src/api/transformers';
 import deepCopy from 'deep-copy';
 import ApplicationsAccess from '@webitel/ui-sdk/src/modules/Userinfo/classes/ApplicationsAccess';
-import instance from '../../../../../app/api/old/instance';
+import instance from '../../../../../app/api/instance';
 
 const baseUrl = '/roles';
 const fieldsToSend = ['name', 'description', 'permissions', 'metadata'];
 
 const preRequestHandler = (item) => {
-  const itemCopy = deepCopy(item);
-  itemCopy.metadata.access = ApplicationsAccess.minify(item.metadata.access);
-  return itemCopy;
+  const copy = deepCopy(item);
+  console.log('copy.metadata.access:', copy.metadata.access);
+  copy.metadata.access = ApplicationsAccess.minify(copy.metadata.access);
+  console.log('copy.metadata.access:', copy.metadata.access);
+  return copy;
 };
 
 // const listGetter = new EndpointListGetterApiConsumer({ baseUrl, instance });
@@ -83,7 +85,34 @@ const getRole = async ({ itemId: id }) => {
   }
 };
 // const extendedRolesListGetter = new EndpointListGetterApiConsumer({ baseUrl, instance });
-const getExtendedRoles = (params) => extendedRolesListGetter.getList(params);
+const getExtendedRoles = async (params) => {
+  const fieldsToSend = ['page', 'size', 'q', 'sort', 'fields', 'id'];
+
+  const url = applyTransform(params, [
+    merge(getDefaultGetParams()),
+    starToSearch('search'),
+    (params) => ({ ...params, q: params.search }),
+    sanitize(fieldsToSend),
+    camelToSnake(),
+    generateUrl(baseUrl),
+  ]);
+  try {
+    const response = await instance.get(url);
+    const { items, next } = applyTransform(response.data, [
+      snakeToCamel(),
+      merge(getDefaultGetListResponse()),
+    ]);
+    return {
+      items,
+      next,
+    };
+  } catch (err) {
+    throw applyTransform(err, [
+      handleUnauthorized,
+      notify,
+    ]);
+  }
+};
 // const itemCreator = new EndpointCreatorApiConsumer({ baseUrl, instance },
 //   { fieldsToSend, preRequestHandler });
 const addRole = async ({ itemInstance }) => {
@@ -146,11 +175,37 @@ const getRolesLookup = (params) => getRoleList({
 });
 
 const PERMISSIONS_LIST_URL = '/permissions';
-const permissionsListGetter = new EndpointListGetterApiConsumer(
-  { baseUrl: PERMISSIONS_LIST_URL, instance },
-);
+// const permissionsListGetter = new EndpointListGetterApiConsumer(
+//   { baseUrl: PERMISSIONS_LIST_URL, instance },
+// );
+const getPermissionsOptions = async (params) => {
+  const fieldsToSend = ['page', 'size', 'q', 'sort', 'fields', 'id'];
 
-const getPermissionsOptions = (payload) => permissionsListGetter.getList(payload);
+  const url = applyTransform(params, [
+    merge(getDefaultGetParams()),
+    starToSearch('search'),
+    (params) => ({ ...params, q: params.search }),
+    sanitize(fieldsToSend),
+    camelToSnake(),
+    generateUrl(PERMISSIONS_LIST_URL),
+  ]);
+  try {
+    const response = await instance.get(url);
+    const { items, next } = applyTransform(response.data, [
+      snakeToCamel(),
+      merge(getDefaultGetListResponse()),
+    ]);
+    return {
+      items,
+      next,
+    };
+  } catch (err) {
+    throw applyTransform(err, [
+      handleUnauthorized,
+      notify,
+    ]);
+  }
+};
 
 const RolesAPI = {
   getList: getRoleList,
