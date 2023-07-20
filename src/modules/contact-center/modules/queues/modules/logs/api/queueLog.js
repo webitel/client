099b5adba4bcf6aec44c1b27eee702a5a8587f07
lizacoma <my.skin00@gmail.com>
2023-07-21 02:01:26 +0300

    refactor: queue logs api

--- src/modules/contact-center/modules/queues/modules/logs/api/queueLog.js
+++ src/modules/contact-center/modules/queues/modules/logs/api/queueLog.js
@@ -1,41 +1,99 @@
 import { MemberServiceApiFactory } from 'webitel-sdk';
-import { SdkListGetterApiConsumer } from 'webitel-sdk/esm2015/api-consumers';
-import instance from '../../../../../../../app/api/old/instance';
+import {
+  getDefaultGetListResponse,
+  getDefaultGetParams,
+} from '@webitel/ui-sdk/src/api/defaults';
+import applyTransform, {
+  handleUnauthorized,
+  merge, notify, sanitize, snakeToCamel,
+  starToSearch,
+} from '@webitel/ui-sdk/src/api/transformers';
+import instance from '../../../../../../../app/api/instance';
 import configuration from '../../../../../../../app/api/openAPIConfig';
 
 const queueMemberAttemptsService = new MemberServiceApiFactory(configuration, '', instance);
 
-const _getQueueLogs = (getList) => function({
-                                              parentId,
-                                              page = 1,
-                                              size = 10,
-                                              search,
-                                              sort = '+joined_at',
-                                              fields,
-                                              joinedAtFrom,
-                                              joinedAtTo,
-                                              result,
-                                              agent,
-                                              leavingAtFrom,
-                                              leavingAtTo,
-                                              offeringAtFrom,
-                                              offeringAtTo,
-                                              durationFrom,
-                                              durationTo,
-                                            }) {
-  // parent id == queue id
-  const params = [
-    page, size, search, sort, fields, joinedAtFrom, joinedAtTo, undefined,
-    parentId, undefined, undefined, agent, result, leavingAtFrom,
-    leavingAtTo, offeringAtFrom, offeringAtTo, durationFrom, durationTo,
+const getQueueLogs = async (params) => {
+  const fieldsToSend = [
+    'page',
+    'size',
+    'search',
+    'sort',
+    'fields',
+    'id',
+    'parentId',
+    'joinedAtFrom',
+    'joinedAtTo',
+    'result',
+    'agent',
+    'leavingAtFrom',
+    'leavingAtTo',
+    'offeringAtFrom',
+    'offeringAtTo',
+    'durationFrom',
+    'durationTo',
   ];
-  return getList(params);
+
+  const {
+    parentId,
+    page = 1,
+    size = 10,
+    search,
+    sort = '+joined_at',
+    fields,
+    joinedAtFrom,
+    joinedAtTo,
+    result,
+    agent,
+    leavingAtFrom,
+    leavingAtTo,
+    offeringAtFrom,
+    offeringAtTo,
+    durationFrom,
+    durationTo,
+  } = applyTransform(params, [
+    merge(getDefaultGetParams()),
+    starToSearch('search'),
+    sanitize(fieldsToSend),
+  ]);
+
+  try {
+    const response = await queueMemberAttemptsService.searchAttemptsHistory(
+      page,
+      size,
+      search,
+      sort,
+      fields,
+      joinedAtFrom,
+      joinedAtTo,
+      undefined,
+      parentId,
+      undefined,
+      undefined,
+      agent,
+      result,
+      leavingAtFrom,
+      leavingAtTo,
+      offeringAtFrom,
+      offeringAtTo,
+      durationFrom,
+      durationTo,
+    );
+    const { items, next } = applyTransform(response.data, [
+      snakeToCamel(),
+      merge(getDefaultGetListResponse()),
+    ]);
+    return {
+      items,
+      next,
+    };
+  } catch (err) {
+    throw applyTransform(err, [
+      handleUnauthorized,
+      notify,
+    ]);
+  }
 };
-
-const listGetter = new SdkListGetterApiConsumer(queueMemberAttemptsService.searchAttemptsHistory)
-.setGetListMethod(_getQueueLogs);
-
-const getQueueLogs = (params) => listGetter.getList(params);
 
 const QueueLogsAPI = {
   getList: getQueueLogs,
