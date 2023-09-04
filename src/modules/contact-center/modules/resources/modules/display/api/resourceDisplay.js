33a52368412ce367b8a257c5ce4b778b47244710
lizacoma <my.skin00@gmail.com>
2023-09-04 23:32:45 +0300

    refactor: resources api [WTEL-3788]

--- src/modules/contact-center/modules/resources/modules/display/api/resourceDisplay.js
+++ src/modules/contact-center/modules/resources/modules/display/api/resourceDisplay.js
@@ -1,33 +1,126 @@
 import { OutboundResourceServiceApiFactory } from 'webitel-sdk';
-import {
-  SdkListGetterApiConsumer,
-  SdkGetterApiConsumer,
-  SdkCreatorApiConsumer,
-  SdkUpdaterApiConsumer,
-  SdkDeleterApiConsumer,
-} from 'webitel-sdk/esm2015/api-consumers';
-import instance from '../../../../../../../app/api/old/instance';
+import applyTransform, {
+  camelToSnake,
+  merge, mergeEach, notify,
+  sanitize, snakeToCamel,
+  starToSearch
+} from '@webitel/ui-sdk/src/api/transformers';
+import instance from '../../../../../../../app/api/instance';
 import configuration from '../../../../../../../app/api/openAPIConfig';
+import { getDefaultGetListResponse, getDefaultGetParams } from '@webitel/ui-sdk/src/api/defaults';
 
 const resService = new OutboundResourceServiceApiFactory(configuration, '', instance);
 
 const fieldsToSend = ['display', 'resourceId'];
 
-const preRequestHandler = (item, parentId) => ({ ...item, resourceId: parentId });
+const preRequestHandler = (parentId) => (item) => ({ ...item, resourceId: parentId });
 
-const listGetter = new SdkListGetterApiConsumer(resService.searchOutboundResourceDisplay);
-const itemGetter = new SdkGetterApiConsumer(resService.readOutboundResourceDisplay);
-const itemCreator = new SdkCreatorApiConsumer(resService.createOutboundResourceDisplay,
-  { fieldsToSend, preRequestHandler });
-const itemUpdater = new SdkUpdaterApiConsumer(resService.updateOutboundResourceDisplay,
-  { fieldsToSend, preRequestHandler });
-const itemDeleter = new SdkDeleterApiConsumer(resService.deleteOutboundResourceDisplay);
+// const listGetter = new SdkListGetterApiConsumer(resService.searchOutboundResourceDisplay);
+const getResDisplayList = async (params) => {
+  const {
+    page,
+    size,
+    search,
+    sort,
+    fields,
+    id,
+    parentId,
+  } = applyTransform(params, [
+    merge(getDefaultGetParams()),
+    starToSearch('search'),
+  ]);
 
-const getResDisplayList = (params) => listGetter.getNestedList(params);
-const getResDisplay = (params) => itemGetter.getNestedItem(params);
-const addResDisplay = (params) => itemCreator.createNestedItem(params);
-const updateResDisplay = (params) => itemUpdater.updateNestedItem(params);
-const deleteResDisplay = (params) => itemDeleter.deleteNestedItem(params);
+  try {
+    const response = await resService.searchOutboundResourceDisplay(
+      parentId,
+      page,
+      size,
+      search,
+      sort,
+      fields,
+      id,
+    );
+    const { items, next } = applyTransform(response.data, [
+      snakeToCamel(),
+      merge(getDefaultGetListResponse()),
+    ]);
+    return {
+      items,
+      next,
+    };
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
+};
+
+// const itemGetter = new SdkGetterApiConsumer(resService.readOutboundResourceDisplay);
+const getResDisplay = async ({ parentId, itemId: id }) => {
+  try {
+    const response = await resService.readOutboundResourceDisplay(parentId, id);
+    return applyTransform(response.data, [
+      snakeToCamel(),
+    ]);
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
+};
+
+// const itemCreator = new SdkCreatorApiConsumer(resService.createOutboundResourceDisplay,
+//   { fieldsToSend, preRequestHandler });
+const addResDisplay = async ({ parentId, itemInstance }) => {
+  const item = applyTransform(itemInstance, [
+    preRequestHandler(parentId),
+    sanitize(fieldsToSend),
+    camelToSnake(),
+  ]);
+  try {
+    const response = await resService.createOutboundResourceDisplay(parentId, item);
+    return applyTransform(response.data, [
+      snakeToCamel(),
+    ]);
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
+};
+
+// const itemUpdater = new SdkUpdaterApiConsumer(resService.updateOutboundResourceDisplay,
+//   { fieldsToSend, preRequestHandler });
+const updateResDisplay = async ({ itemInstance, itemId: id, parentId }) => {
+  const item = applyTransform(itemInstance, [
+    preRequestHandler(parentId),
+    sanitize(fieldsToSend),
+    camelToSnake(),
+  ]);
+  try {
+    const response = await resService.updateOutboundResourceDisplay(parentId, id, item);
+    return applyTransform(response.data, [
+      snakeToCamel(),
+    ]);
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
+};
+
+// const itemDeleter = new SdkDeleterApiConsumer(resService.deleteOutboundResourceDisplay);
+const deleteResDisplay = async ({ parentId, id }) => {
+  try {
+    const response = await resService.deleteOutboundResourceDisplay(parentId, id);
+    return applyTransform(response.data, []);
+  } catch (err) {
+    throw applyTransform(err, [
+
+      notify,
+    ]);
+  }
+};
 
 export default {
   getList: getResDisplayList,
