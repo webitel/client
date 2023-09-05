8d6a84d872185a536be65b1a057a66329940d31a
liza-pohranichna <haemoglobin0@gmail.com>
2023-09-05 18:25:47 +0300

    refactor: res-groups api [WTEL-3788]

--- src/modules/contact-center/modules/resource-groups/api/resourceGroups.js
+++ src/modules/contact-center/modules/resource-groups/api/resourceGroups.js
@@ -1,64 +1,181 @@
 import { OutboundResourceGroupServiceApiFactory } from 'webitel-sdk';
 import {
-  SdkListGetterApiConsumer,
-  SdkGetterApiConsumer,
-  SdkCreatorApiConsumer,
-  SdkUpdaterApiConsumer,
-  SdkDeleterApiConsumer,
-} from 'webitel-sdk/esm2015/api-consumers';
-import instance from '../../../../../app/api/old/instance';
+  getDefaultGetListResponse,
+  getDefaultGetParams,
+} from '@webitel/ui-sdk/src/api/defaults';
+import applyTransform, {
+  camelToSnake,
+  merge, notify, sanitize, snakeToCamel,
+  starToSearch, log,
+} from '@webitel/ui-sdk/src/api/transformers';
+import deepCopy from 'deep-copy';
+import instance from '../../../../../app/api/instance';
 import configuration from '../../../../../app/api/openAPIConfig';
 
 const resGrService = new OutboundResourceGroupServiceApiFactory(configuration, '', instance);
 
 const fieldsToSend = ['name', 'description', 'strategy', 'communication', 'time'];
 
-const defaultSingleObject = {
-  name: '',
-  strategy: '',
-  description: '',
-  communication: {},
-  time: [
-    {
-      start: 0,
-      finish: 0,
-    },
-  ],
+// const defaultSingleObject = {
+//   name: '',
+//   strategy: '',
+//   description: '',
+//   communication: {},
+//   time: [
+//     {
+//       start: 0,
+//       finish: 0,
+//     },
+//   ],
+// };
+
+
+const preRequestHandler = (item) => {
+  const copy = deepCopy(item);
+  copy.time = copy.time.map((range) => ({
+    startTimeOfDay: range.start,
+    endTimeOfDay: range.end,
+  }));
+  return copy;
 };
 
-const itemResponseHandler = (response) => {
+// const listGetter = new SdkListGetterApiConsumer(resGrService.searchOutboundResourceGroup);
+const getResGroupList = async (params) => {
+  const {
+    page,
+    size,
+    search,
+    sort,
+    fields,
+    id,
+  } = applyTransform(params, [
+    merge(getDefaultGetParams()),
+    starToSearch('search'),
+  ]);
+
+  try {
+    const response = await resGrService.searchOutboundResourceGroup(
+      page,
+      size,
+      search,
+      sort,
+      fields,
+      id,
+    );
+    const { items, next } = applyTransform(response.data, [
+      snakeToCamel(),
+      merge(getDefaultGetListResponse()),
+    ]);
+    return {
+      items,
+      next,
+    };
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
+};
+
+
+// const itemGetter = new SdkGetterApiConsumer(resGrService.readOutboundResourceGroup,
+//   { defaultSingleObject, itemResponseHandler });
+const getResGroup = async ({ itemId: id }) => {
+  const defaultObject = {
+      name: '',
+      strategy: '',
+      description: '',
+      communication: {},
+      time: [
+        {
+          start: 0,
+          finish: 0,
+        },
+      ],
+  };
+
+  const responseHandler = (response) => {
     const time = response.time.map((range) => ({
       start: range.startTimeOfDay || 0,
       end: range.endTimeOfDay || 0,
     }));
-
     return { ...response, time };
+  };
+
+  try {
+    const response = await resGrService.readOutboundResourceGroup(id);
+    return applyTransform(response.data, [
+      snakeToCamel(),
+      merge(defaultObject),
+      responseHandler,
+    ]);
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
 };
 
-const preRequestHandler = (item) => {
-  // eslint-disable-next-line no-param-reassign
-  item.time = item.time.map((range) => ({
-    startTimeOfDay: range.start,
-    endTimeOfDay: range.end,
-  }));
-  return item;
+
+// const itemCreator = new SdkCreatorApiConsumer(resGrService.createOutboundResourceGroup,
+//   { fieldsToSend, preRequestHandler });
+const addResGroup = async ({ itemInstance }) => {
+  const item = applyTransform(itemInstance, [
+    preRequestHandler,
+    sanitize(fieldsToSend),
+    camelToSnake(),
+  ]);
+  try {
+    const response = await resGrService.createOutboundResourceGroup(item);
+    return applyTransform(response.data, [
+      snakeToCamel(),
+    ]);
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
 };
 
-const listGetter = new SdkListGetterApiConsumer(resGrService.searchOutboundResourceGroup);
-const itemGetter = new SdkGetterApiConsumer(resGrService.readOutboundResourceGroup,
-  { defaultSingleObject, itemResponseHandler });
-const itemCreator = new SdkCreatorApiConsumer(resGrService.createOutboundResourceGroup,
-  { fieldsToSend, preRequestHandler });
-const itemUpdater = new SdkUpdaterApiConsumer(resGrService.updateOutboundResourceGroup,
-  { fieldsToSend, preRequestHandler });
-const itemDeleter = new SdkDeleterApiConsumer(resGrService.deleteOutboundResourceGroup);
 
-const getResGroupList = (params) => listGetter.getList(params);
-const getResGroup = (params) => itemGetter.getItem(params);
-const addResGroup = (params) => itemCreator.createItem(params);
-const updateResGroup = (params) => itemUpdater.updateItem(params);
-const deleteResGroup = (params) => itemDeleter.deleteItem(params);
-const getResGroupsLookup = (params) => listGetter.getLookup(params);
+// const itemUpdater = new SdkUpdaterApiConsumer(resGrService.updateOutboundResourceGroup,
+//   { fieldsToSend, preRequestHandler });
+const updateResGroup = async ({ itemInstance, itemId: id }) => {
+  const item = applyTransform(itemInstance, [
+    preRequestHandler,
+    log,
+    sanitize(fieldsToSend),
+    camelToSnake(),
+  ]);
+  try {
+    const response = await resGrService.updateOutboundResourceGroup(id, item);
+    return applyTransform(response.data, [
+      snakeToCamel(),
+    ]);
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
+};
+
+
+const deleteResGroup = async ({ id }) => {
+  try {
+    const response = await resGrService.deleteOutboundResourceGroup(id);
+    return applyTransform(response.data, []);
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
+};
+
+
+const getResGroupsLookup = (params) => getResGroupList({
+   ...params,
+   fields: params.fields || ['id', 'name'],
+  });
 
 const ResourceGroupsAPI = {
   getList: getResGroupList,
