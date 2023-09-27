3761195bf80fd58b84f67513561d91a7c2273f14
Lera24 <bedulinavalera21@gmail.com>
2023-09-27 15:30:01 +0300

    fix: display dummy in last page[WTEL-3793]

--- src/app/store/BaseStoreModules/StoreModuleMixins/BaseTableStoreModuleMixin.js
+++ src/app/store/BaseStoreModules/StoreModuleMixins/BaseTableStoreModuleMixin.js
@@ -36,10 +36,10 @@ const actions = {
         aggs = {},
       } = await context.dispatch('GET_LIST', query);
 
-      /* [WTEL-3793]
+      /* [https://my.webitel.com/browse/WTEL-3793]
       * When deleting the last item from list,
       * if there are other items on the previous page, you need to go back */
-      if (!items.length && context.state.page > 1) context.dispatch('PREV_PAGE');
+      if (!items.length && context.state.page > 1) return context.dispatch('PREV_PAGE');
 
       /* we should set _isSelected property to all items in tables cause their checkbox selection
       * is based on this property. Previously, this prop was set it api consumers, but now
