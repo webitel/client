fcc48545908d6a6e7b8a3c6125f2dbce4db83900
lizacoma <my.skin00@gmail.com>
2023-11-06 23:31:48 +0200

    refactor: objects api in progress [WTEL-4023]

--- src/modules/permissions/modules/objects/modules/rbac/api/objectsRbac.js
+++ src/modules/permissions/modules/objects/modules/rbac/api/objectsRbac.js
@@ -1,8 +1,18 @@
 import {
-  EndpointListGetterApiConsumer,
-  EndpointPatcherApiConsumer,
-} from 'webitel-sdk/esm2015/api-consumers';
-import instance from '../../../../../../../app/api/old/instance';
+  getDefaultGetListResponse,
+  getDefaultGetParams,
+} from '@webitel/ui-sdk/src/api/defaults';
+import applyTransform, {
+  camelToSnake,
+  generateUrl,
+  merge,
+  mergeEach,
+  notify,
+  sanitize,
+  snakeToCamel,
+  starToSearch,
+} from '@webitel/ui-sdk/src/api/transformers';
+import instance from '../../../../../../../app/api/instance';
 import APIPermissionsGetter
   from '../../../../../../../app/api/old/PermissionsAPIService/APIPermissionsGetter';
 
@@ -13,13 +23,49 @@ const _getObjclassDefaultList = (method) => function(params) {
   return method(params, baseUrl);
 };
 
-const listGetter = new EndpointListGetterApiConsumer({ baseUrl, instance }, {
-  listResponseHandler: APIPermissionsGetter.handlePermissionsListResponse,
-}).setGetListMethod(_getObjclassDefaultList);
-const itemPatcher = new EndpointPatcherApiConsumer({
-  baseUrl,
-  instance,
-}, { nestedUrl: 'grantor' });
+// const listGetter = new EndpointListGetterApiConsumer({ baseUrl, instance }, {
+//   listResponseHandler: APIPermissionsGetter.handlePermissionsListResponse,
+// }).setGetListMethod(_getObjclassDefaultList);
+export const getObjclassDefaultList = async (params) => {
+  const fieldsToSend = ['page', 'size', 'q', 'sort', 'fields', 'id'];
 
-export const getObjclassDefaultList = (params) => listGetter.getList(params);
+  const defaultObject = {
+    // default object prototype, to merge response with it to get all fields
+    class: '',
+    obac: false,
+    rbac: false,
+    id: 0,
+  };
+
+  const url = applyTransform(params, [
+    merge(getDefaultGetParams()),
+    starToSearch('search'),
+    (params) => ({ ...params, q: params.search }),
+    sanitize(fieldsToSend),
+    camelToSnake(),
+    generateUrl(baseUrl),
+  ]);
+  try {
+    const response = await instance.get(url);
+    const { items, next } = applyTransform(response.data, [
+      snakeToCamel(),
+      merge(getDefaultGetListResponse()),
+    ]);
+    return {
+      items: applyTransform(items, [
+        mergeEach(defaultObject),
+      ]),
+      next,
+    };
+  } catch (err) {
+    throw applyTransform(err, [
+      notify,
+    ]);
+  }
+};
+
+// const itemPatcher = new EndpointPatcherApiConsumer({
+//   baseUrl,
+//   instance,
+// }, { nestedUrl: 'grantor' });
 export const patchObjclassDefaultMode = (params) => itemPatcher.patchNestedItem(params);
