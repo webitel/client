493a0d561bb9416c07b0ae067cebc876f96d8f18
volodymyr <volodymyr.dechtiaruk@gmail.com>
2023-11-20 12:32:01 +0200

    refactor: tests fixed [ENPF-136]

--- src/modules/integrations/modules/triggers/components/__tests__/the-triggers.spec.js
+++ src/modules/integrations/modules/triggers/components/__tests__/the-triggers.spec.js
@@ -1,16 +1,16 @@
+import { mount } from '@vue/test-utils';
 import { reactive } from 'vue';
-import { shallowMount } from '@vue/test-utils';
-import TheTriggers from '../the-triggers.vue';
 import tableComponentComputedMock
   from '../../../../../../../tests/unit/mocks/tableComponentComputed.mock';
 import { useDummy } from '../../../../../../app/composables/useDummy';
+import TheTriggers from '../the-triggers.vue';
 
 jest.mock('../../../../../../app/composables/useDummy');
 useDummy.mockImplementation(() => ({ store: reactive({}) }));
 
 describe('TheTriggers', () => {
   it('renders a component', () => {
-    const wrapper = shallowMount(TheTriggers, { computed: tableComponentComputedMock() });
+    const wrapper = mount(TheTriggers, { computed: tableComponentComputedMock() });
     expect(wrapper.exists()).toBe(true);
   });
 });
