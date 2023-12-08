a0459af5263a1710162b0de6cd575cb133a8a622
dlohvinov <nsblnr@gmail.com>
2023-12-08 11:48:21 +0200

    fix: tests [WTEL-3902]

--- src/modules/routing/modules/flow/components/__tests__/create-flow-popup.spec.js
+++ src/modules/routing/modules/flow/components/__tests__/create-flow-popup.spec.js
@@ -1,9 +1,21 @@
 import { shallowMount } from '@vue/test-utils';
+import { createStore } from 'vuex';
+import appearance from '../../../../../appearance/store/appearance';
 import CreateFlowPopup from '../create-flow-popup.vue';
 
 describe('CreateFlowPopup', () => {
+  const store = createStore({
+    modules: {
+      appearance,
+    },
+  });
+
   it('renders a component', () => {
-    const wrapper = shallowMount(CreateFlowPopup);
+    const wrapper = shallowMount(CreateFlowPopup, {
+      global: {
+        plugins: [store],
+      },
+    });
     expect(wrapper.isVisible()).toBe(true);
   });
 });
