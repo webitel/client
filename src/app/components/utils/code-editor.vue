0660f588d7e4056ae3240ee9635abb43e061c66e
Lera24 <bedulinavalera21@gmail.com>
2024-01-03 15:12:29 +0200

    fix: error in codeEditor component

--- src/app/components/utils/code-editor.vue
+++ src/app/components/utils/code-editor.vue
@@ -27,7 +27,29 @@
 import { editor, languages } from 'monaco-editor';
 import { markRaw } from 'vue';
 import { mapState } from 'vuex';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === 'json') {
+      return new jsonWorker()
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return new cssWorker()
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return new htmlWorker()
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return new tsWorker()
+    }
+    return new editorWorker()
+  }
+}
 // https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ieditorconstructionoptions.html
 
 const defaultSizeConfig = {
