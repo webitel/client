6cae09d51360b2c397e2987ab4318cce0820530b
dlohvinov <nsblnr@gmail.com>
2024-01-17 16:23:05 +0200

    feat: amd non human config [WTEL-4010]

--- src/modules/system/modules/configuration/utils/configurationValueTypes.js
+++ src/modules/system/modules/configuration/utils/configurationValueTypes.js
@@ -2,6 +2,7 @@ import { EngineSystemSettingName } from 'webitel-sdk';
 
 const ConfigurationValueTypes = Object.freeze({
     [EngineSystemSettingName.EnableOmnichannel]: 'boolean',
+    [EngineSystemSettingName.AmdCancelNotHuman]: 'boolean',
     [EngineSystemSettingName.MemberChunkSize]: 'number',
   },
 );
