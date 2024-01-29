8190ba0af87cd3565902679beb0f6e085a0f9bda
dlohvinov <nsblnr@gmail.com>
2024-01-29 16:20:48 +0200

    fix: queue filters layout [WTEL-3222]

--- src/modules/contact-center/modules/queues/modules/filters/components/the-queues-filters.vue
+++ src/modules/contact-center/modules/queues/modules/filters/components/the-queues-filters.vue
@@ -4,27 +4,28 @@
       :namespace="namespace"
     />
     <component
-      class="queues-abstract-filter"
       :is="`abstract-${filter.type}-filter`"
       v-for="(filter) of filters"
       :key="filter.filterQuery"
       :filter-query="filter.filterQuery"
       :namespace="namespace"
+      class="queues-abstract-filter"
     />
   </wt-filters-panel-wrapper>
 </template>
 
 <script>
+import AbstractApiFilter from '@webitel/ui-sdk/src/modules/QueryFilters/components/abstract-api-filter.vue';
 import AbstractEnumFilter from '@webitel/ui-sdk/src/modules/QueryFilters/components/abstract-enum-filter.vue';
 import { mapActions } from 'vuex';
-import AbstractApiFilter from '@webitel/ui-sdk/src/modules/QueryFilters/components/abstract-api-filter.vue';
 import QueuesTagsFilter from './queues-tags-filter.vue';
+
 export default {
   name: 'TheQueuesFilters',
   components: {
     QueuesTagsFilter,
     AbstractApiFilter,
-    AbstractEnumFilter
+    AbstractEnumFilter,
   },
   props: {
     namespace: {
@@ -56,7 +57,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.queues-abstract-filter, .queues-tags-filter {
-  grid-column: span 2;
-}
+
 </style>
