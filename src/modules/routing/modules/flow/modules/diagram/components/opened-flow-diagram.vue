0078959b4b678d9cda44401f07ab938eeac351c5
Lera24 <bedulinavalera21@gmail.com>
2024-05-03 14:54:11 +0300

    fix: opened flow in new tab [WTEL-4509]

--- src/modules/routing/modules/flow/modules/diagram/components/opened-flow-diagram.vue
+++ src/modules/routing/modules/flow/modules/diagram/components/opened-flow-diagram.vue
@@ -11,7 +11,12 @@ export default {
     const flowId = this.$route.params.id || 'new';
     const flowType = this.$route.query.type || '';
     const redirectUrl = `${import.meta.env.VITE_FLOW_DIAGRAM_URL}/${flowId}/${flowType}`;
-    window.location.replace(redirectUrl);
+
+    // [https://webitel.atlassian.net/browse/WTEL-4509]
+    // Temporary solution - open in a new browser tab Flow diagram
+
+    // window.location.replace(redirectUrl);
+    window.open(redirectUrl, '_blank');
   },
 };
 </script>
