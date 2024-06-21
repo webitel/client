31c746ebe105f6b849f90bcc05a5ffaf26ae510b
volodymyr <volodymyr.dechtiaruk@gmail.com>
2024-06-21 18:41:14 +0300

    refactor: config refactor [WTEL-4744]

--- src/modules/system/modules/configuration/utils/configurationValueTypes.js
+++ src/modules/system/modules/configuration/utils/configurationValueTypes.js
@@ -1,11 +1,14 @@
 import { EngineSystemSettingName } from 'webitel-sdk';
 
 const ConfigurationValueTypes = Object.freeze({
-  [EngineSystemSettingName.EnableOmnichannel]: 'boolean',
-  [EngineSystemSettingName.AmdCancelNotHuman]: 'boolean',
-  [EngineSystemSettingName.Enable2fa]: 'boolean',
-  [EngineSystemSettingName.MemberChunkSize]: 'number',
-  [EngineSystemSettingName.SchemeVersionLimit]: 'number',
-});
+    [EngineSystemSettingName.EnableOmnichannel]: 'boolean',
+    [EngineSystemSettingName.AmdCancelNotHuman]: 'boolean',
+    [EngineSystemSettingName.Enable2fa]: 'boolean',
+    [EngineSystemSettingName.MemberChunkSize]: 'number',
+    [EngineSystemSettingName.SchemeVersionLimit]: 'number',
+    [EngineSystemSettingName.SearchNumberLength]: 'number',
+    [EngineSystemSettingName.ExportSettings]: 'select',
+  },
+);
 
 export default ConfigurationValueTypes;
