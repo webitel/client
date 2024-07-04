ea9d6f887e5b3e3d6105de1a9da71eb756546a23
Sviatoslav Bar <baterfild2011@gmail.com>
2024-07-04 10:33:53 +0300

    Feat/ add dummy image. ccenter --> queues --> id --> members [WTEL-3356]

--- src/app/locale/en/en.js
+++ src/app/locale/en/en.js
@@ -791,7 +791,7 @@ export default {
         tags: 'Tags',
         newQueue: 'New queue',
         blacklist: 'Stop list',
-        newQueueDescription: 'New queue descriotion',
+        newQueueDescription: 'New queue description',
         outboundIVRQueue: 'Outbound IVR queue',
         outboundIVR: 'Outbound IVR',
         outboundIVRQueueDescription:
