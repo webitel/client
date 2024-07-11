0f662bf62fec53d3a26572958ba1cea8fa6596a5
RomanZaritskyi <romanzaritskyi@gmail.com>
2024-07-11 12:32:28 +0300

    Separate routes into modules [WTEL-4559]

--- src/app/router/router.js
+++ src/app/router/router.js
@@ -1,25 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import CognitiveProfilesRoutes
-  from '../../modules/integrations/modules/cognitive-profiles/router/cognitiveProfiles.js';
-import EmailProfilesRoutes
-  from '../../modules/integrations/modules/email-profiles/router/emailProfiles.js';
-import ImportCsvRoutes
-  from '../../modules/integrations/modules/import-csv/router/importCsv.js';
-import TriggersRoutes
-  from '../../modules/integrations/modules/triggers/router/triggers.js';
+import CognitiveProfilesRoutes from '../../modules/integrations/modules/cognitive-profiles/router/cognitiveProfiles.js';
+import EmailProfilesRoutes from '../../modules/integrations/modules/email-profiles/router/emailProfiles.js';
+import ImportCsvRoutes from '../../modules/integrations/modules/import-csv/router/importCsv.js';
+import TriggersRoutes from '../../modules/integrations/modules/triggers/router/triggers.js';
 import RolesRoutes from '../../modules/permissions/modules/roles/router/roles.js';
-import ChangeLogsRoutes
-  from '../../modules/system/modules/changelogs/router/changelogs.js';
-import ConfigurationRoutes
-  from '../../modules/system/modules/configuration/router/configuration.js';
-import GlobalVariablesRoutes
-  from '../../modules/system/modules/global-variables/router/globalVariables.js';
+import ChangeLogsRoutes from '../../modules/system/modules/changelogs/router/changelogs.js';
+import ConfigurationRoutes from '../../modules/system/modules/configuration/router/configuration.js';
+import GlobalVariablesRoutes from '../../modules/system/modules/global-variables/router/globalVariables.js';
 import RouteNames from './_internals/RouteNames.enum';
-import LicencesRouteNames from './_internals/tabs/directory/LicencesRouteNames.enum.js';
 import AgentSkillsRouteNames from './_internals/tabs/lookups/AgentSkillsRouteNames.enum.js';
-import ChatGatewayRouteNames from './_internals/tabs/routing/ChatGatewayRouteNames.enum.js';
 import ChatplanRouteNames from './_internals/tabs/routing/ChatplanRouteNames.enum.js';
-import FlowRouteNames from './_internals/tabs/routing/FlowRouteNames.enum.js';
 import DialplanRouteNames from './_internals/tabs/routing/DialplanRouteNames.enum.js';
 import GatewaysRouteNames from './_internals/tabs/routing/GatewaysRouteNames.enum.js';
 import BucketsRouteNamesEnum from './_internals/tabs/lookups/BucketsRouteNames.enum.js';
@@ -28,6 +18,18 @@ import RegionRouteNamesEnum from './_internals/tabs/lookups/RegionRouteNames.enu
 import CalendarRouteNamesEnum from './_internals/tabs/lookups/CalendarRouteNames.enum.js';
 import CommunicationsRouteNamesEnum from './_internals/tabs/lookups/CommunicationsRouteNames.enum.js';
 import AgentPauseCauseRouteNamesEnum from './_internals/tabs/lookups/AgentPauseCauseRouteNames.enum.js';
+import UsersRoutes from '../../modules/directory/modules/users/router/users.js';
+import DevicesRoutes from '../../modules/directory/modules/devices/router/devices.js';
+import AgentRoutes from "../../modules/contact-center/modules/agents/router/agents.js";
+import TeamsRoutes from '../../modules/contact-center/modules/teams/router/teams.js';
+import ResourcesRoutes from '../../modules/contact-center/modules/resources/router/resources.js';
+import ResourcesGroupRoutes from '../../modules/contact-center/modules/resource-groups/router/resourceGroup.js';
+import QueuesRoutes from '../../modules/contact-center/modules/queues/router/queues.js';
+import StorageRoutes from '../../modules/integrations/modules/storage/router/storage.js';
+import ObjectsRoutes from "../../modules/permissions/modules/objects/router/objects.js";
+import LicenseRoutes from "../../modules/directory/modules/license/router/license.js";
+import FlowRoutes from "../../modules/routing/modules/flow/router/flow.js";
+import ChatGetewaysRoutes from "../../modules/routing/modules/chat-gateways/router/chatGeteways.js";
 
 const ApplicationHub = () => import('../../modules/application-hub/components/application-hub.vue');
 const ModuleWrap = () => import('../../modules/_shared/object-wrap/the-object-wrap.vue');
@@ -35,7 +37,6 @@ const StartPage = () => import('../../modules/start-page/components/the-start-pa
 const Settings = () => import('../../modules/settings/components/the-settings.vue');
 const AccessDenied = () => import('../../modules/error-pages/components/the-access-denied-component.vue');
 const NotFound = () => import('../../modules/error-pages/components/the-not-found-component.vue');
-const License = () => import('../../modules/directory/modules/license/components/the-license.vue');
 const Blacklists = () => import('../../modules/lookups/modules/blacklists/components/the-blacklists.vue');
 const Media = () => import('../../modules/lookups/modules/media/components/the-media.vue');
 const OpenedBlacklist = () => import('../../modules/lookups/modules/blacklists/components/opened-blacklist.vue');
@@ -49,10 +50,7 @@ const AgentPauseCause = () => import('../../modules/lookups/modules/agent-pause-
 const OpenedAgentPauseCause = () => import('../../modules/lookups/modules/agent-pause-cause/components/opened-agent-pause-cause.vue');
 const SipGateways = () => import('../../modules/routing/modules/gateways/components/the-sip-gateways.vue');
 const OpenedGateway = () => import('../../modules/routing/modules/gateways/components/opened-sip-gateway.vue');
-const ChatGateways = () => import('../../modules/routing/modules/chat-gateways/components/the-chat-gateways.vue');
-const OpenedChatGateways = () => import('../../modules/routing/modules/chat-gateways/components/opened-chat-gateway.vue');
-const Flow = () => import('../../modules/routing/modules/flow/components/the-flow.vue');
-const OpenedFlow = () => import('../../modules/routing/modules/flow/components/opened-flow.vue');
+
 const Dialplan = () => import('../../modules/routing/modules/dialplan/components/the-dialplan.vue');
 const OpenedDialplan = () => import('../../modules/routing/modules/dialplan/components/opened-dialplan.vue');
 const Chatplan = () => import('../../modules/routing/modules/chatplan/components/the-chatplan.vue');
@@ -61,31 +59,13 @@ const AgentSkills = () => import('../../modules/lookups/modules/agent-skills/com
 const OpenedAgentSkill = () => import('../../modules/lookups/modules/agent-skills/components/opened-agent-skill.vue');
 const Buckets = () => import('../../modules/lookups/modules/buckets/components/the-buckets.vue');
 const OpenedBucket = () => import('../../modules/lookups/modules/buckets/components/opened-bucket.vue');
-const AllLicenses = () => import('../../modules/directory/modules/license/components/all-licenses/all-licenses.vue');
-const LicensesByUser = () => import('../../modules/directory/modules/license/modules/users/components/licenses-by-user.vue');
 const PermissionsTab = () => import('../../modules/_shared/permissions-tab/components/permissions-tab.vue');
 const OpenedDialplanGeneral = () => import('../../modules/routing/modules/dialplan/components/opened-dialplan-general.vue');
-const OpenedFlowDiagram = () => import('../../modules/routing/modules/flow/modules/diagram/components/opened-flow-diagram.vue');
 const OpenedRegisterSipGatewayGeneral = () => import('../../modules/routing/modules/gateways/components/opened-register-sip-gateway-general.vue');
 const OpenedTrunkingSipGatewayGeneral = () => import('../../modules/routing/modules/gateways/components/opened-trunking-sip-gateway-general.vue');
 const OpenedTrunkingSipGatewayConfiguration =  () => import('../../modules/routing/modules/gateways/components/opened-trunking-sip-gateway-configuration.vue');
 const OpenedChatplanGeneral = () => import('../../modules/routing/modules/chatplan/components/opened-chatplan-general.vue');
-const OpenedChatGatewayTelegramBotGeneralTab = () => import('../../modules/routing/modules/chat-gateways/components/telegram-bot/opened-chat-gateway-telegram-bot-general-tab.vue');
-const OpenedChatGatewayTelegramAppGeneralTab = () => import('../../modules/routing/modules/chat-gateways/components/telegram-app/opened-chat-gateway-telegram-app-general-tab.vue');
-const OpenedChatGatewayTemplatesTab = () => import('../../modules/routing/modules/chat-gateways/components/_shared/opened-chat-gateway-templates-tab.vue');
-const OpenedChatGatewayMessengerGeneralTab = () => import('../../modules/routing/modules/chat-gateways/components/messenger/opened-chat-gateway-messenger-general-tab.vue');
-const OpenedChatFacebook = () => import('../../modules/routing/modules/chat-gateways/modules/messenger/facebook/components/facebook-tab.vue');
-const OpenedChatInstagram = () => import('../../modules/routing/modules/chat-gateways/modules/messenger/instagram/components/instagram-tab.vue');
-const OpenedChatWhatsapp = () => import('../../modules/routing/modules/chat-gateways/modules/messenger/whatsapp/components/whatsapp-tab.vue');
-const OpenedChatGatewayInfobipGeneralTab = () => import('../../modules/routing/modules/chat-gateways/components/infobip/opened-chat-gateway-infobip-general-tab.vue');
-const OpenedChatGatewayViberGeneralTab = () => import('../../modules/routing/modules/chat-gateways/components/viber/opened-chat-gateway-viber-general-tab.vue');
-const OpenedChatGatewayWebchatGeneralTab = () => import('../../modules/routing/modules/chat-gateways/modules/webchat/components/opened-chat-gateway-webchat-general-tab.vue');
-const OpenedChatGatewayWebchatViewTab = () => import('../../modules/routing/modules/chat-gateways/modules/webchat/components/opened-chat-gateway-webchat-view-tab.vue');
-const OpenedChatGatewayWebchatAlternativeChannelsTab = () => import('../../modules/routing/modules/chat-gateways/modules/webchat/components/opened-chat-gateway-webchat-alternative-channels-tab.vue');
-const OpenedChatGatewayWebchatChatTab = () => import('../../modules/routing/modules/chat-gateways/modules/webchat/components/opened-chat-gateway-webchat-chat-tab.vue');
-const OpenedChatGatewayWebchatAppointmentTab = () => import('../../modules/routing/modules/chat-gateways/modules/webchat/components/opened-chat-gateway-webchat-appointment-tab.vue');
-const OpenedChatGatewayWebchatRecaptchaTab = () => import('../../modules/routing/modules/chat-gateways/modules/webchat/components/opened-chat-gateway-webchat-recaptcha-tab.vue');
-const OpenedChatGatewayCustomGeneralTab = () => import('../../modules/routing/modules/chat-gateways/components/custom/opened-chat-gateway-custom-general-tab.vue');
+
 const OpenedAgentSkillGeneral = () => import('../../modules/lookups/modules/agent-skills/components/opened-agent-skill-general.vue');
 const OpenedSkillAgents = () => import('../../modules/lookups/modules/agent-skills/modules/agents/components/opened-skill-agents.vue');
 const OpenedBucketGeneral = () => import('../../modules/lookups/modules/buckets/components/opened-bucket-general.vue');
@@ -97,17 +77,8 @@ const OpenedCalendarHolidays = () => import('../../modules/lookups/modules/calen
 const OpenedCalendarWorkingWeek = () => import('../../modules/lookups/modules/calendars/components/opened-calendar-work-week.vue');
 const OpenedCommunicationTypeGeneral = () => import('../../modules/lookups/modules/communications/components/opened-communication-type-general.vue');
 const OpenedAgentPauseCauseGeneral = () => import('../../modules/lookups/modules/agent-pause-cause/components/opened-agent-pause-cause-general.vue');
-import UsersRoutes from '../../modules/directory/modules/users/router/users.js';
-import DevicesRoutes from '../../modules/directory/modules/devices/router/devices.js';
-import AgentRoutes from "../../modules/contact-center/modules/agents/router/agents.js";
-import TeamsRoutes from '../../modules/contact-center/modules/teams/router/teams.js';
-import ResourcesRoutes from '../../modules/contact-center/modules/resources/router/resources.js';
-import ResourcesGroupRoutes from '../../modules/contact-center/modules/resource-groups/router/resourceGroup.js';
-import QueuesRoutes from '../../modules/contact-center/modules/queues/router/queues.js';
-import StorageRoutes from '../../modules/integrations/modules/storage/router/storage.js';
 
 import { checkAppAccess, checkRouteAccess } from './_internals/guards.js';
-import ObjectsRoutes from "../../modules/permissions/modules/objects/router/objects.js";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -149,56 +120,13 @@ const router = createRouter({
 
         // ----------DIRECTORY------------
         ...DevicesRoutes,
-        {
-          path: '/directory/license',
-          name: RouteNames.LICENSE,
-          component: License,
-          redirect: {name: `${LicencesRouteNames.ALL}-card`},
-          beforeEnter: checkRouteAccess,
-          children: [
-            {
-              path: 'all/:id?',
-              name: `${LicencesRouteNames.ALL}-card`,
-              component: AllLicenses,
-            },{
-              path: 'by-user',
-              name: LicencesRouteNames.BY_USER,
-              component: LicensesByUser,
-            }
-          ],
-        },
+        ...LicenseRoutes,
         ...UsersRoutes,
         // ----------DIRECTORY END------------
 
         // ----------ROUTING------------
-        {
-          path: '/routing/flow',
-          name: RouteNames.FLOW,
-          component: Flow,
-          beforeEnter: checkRouteAccess,
-          children: [
-            {
-              path: 'uploadCSV',
-              name: FlowRouteNames.UPLOAD_CSV,
-              meta: { uploadCSV: true },
-              component: Flow,
-            }
-          ],
-        },
-        {
-          path: '/routing/flow/:id',
-          name: `${RouteNames.FLOW}-card`,
-          redirect: { name: FlowRouteNames.GENERAL },
-          component: OpenedFlow,
-          beforeEnter: checkRouteAccess,
-          children: [
-            {
-              path: 'general',
-              name: FlowRouteNames.GENERAL,
-              component: OpenedFlowDiagram,
-            }
-          ],
-        },
+        ...FlowRoutes,
+        ...ChatGetewaysRoutes,
         {
           path: '/routing/dialplan',
           name: RouteNames.DIALPLAN,
@@ -267,86 +195,7 @@ const router = createRouter({
             }
           ],
         },
-        {
-          path: '/routing/chat-gateways',
-          name: RouteNames.CHAT_GATEWAYS,
-          component: ChatGateways,
-          beforeEnter: checkRouteAccess,
-        },
-        {
-          path: '/routing/chat-gateways/:id',
-          name: `${RouteNames.CHAT_GATEWAYS}-card`,
-          redirect: { name: ChatGatewayRouteNames.GENERAL },
-          component: OpenedChatGateways,
-          beforeEnter: checkRouteAccess,
-          children: [
-            {
-              path: 'telegram-bot',
-              name: ChatGatewayRouteNames.GENERAL,
-              component: OpenedChatGatewayTelegramBotGeneralTab,
-            },{
-              path: 'telegram-app',
-              name: ChatGatewayRouteNames.GENERAL,
-              component: OpenedChatGatewayTelegramAppGeneralTab,
-            },{
-              path: 'infobip',
-              name: ChatGatewayRouteNames.GENERAL,
-              component: OpenedChatGatewayInfobipGeneralTab,
-            },{
-              path: 'meta',
-              name: ChatGatewayRouteNames.GENERAL,
-              component: OpenedChatGatewayMessengerGeneralTab,
-            },{
-              path: 'facebook-pages',
-              name: ChatGatewayRouteNames.FACEBOOK_PAGES,
-              component: OpenedChatFacebook,
-            },{
-              path: 'instagram',
-              name: ChatGatewayRouteNames.INSTAGRAM,
-              component: OpenedChatInstagram,
-            },{
-              path: 'whatsapp',
-              name: ChatGatewayRouteNames.WHATSAPP,
-              component: OpenedChatWhatsapp,
-            },{
-              path: 'viber',
-              name: ChatGatewayRouteNames.GENERAL,
-              component: OpenedChatGatewayViberGeneralTab,
-            },{
-              path: 'general',
-              name: ChatGatewayRouteNames.GENERAL,
-              component: OpenedChatGatewayWebchatGeneralTab,
-            },{
-              path: 'view',
-              name: ChatGatewayRouteNames.VIEW,
-              component: OpenedChatGatewayWebchatViewTab,
-            },{
-              path: 'chat',
-              name: ChatGatewayRouteNames.CHAT,
-              component: OpenedChatGatewayWebchatChatTab,
-            },{
-              path: 'appointment',
-              name: ChatGatewayRouteNames.APPOINTMENT,
-              component: OpenedChatGatewayWebchatAppointmentTab,
-            },{
-              path: 'alternative-channels',
-              name: ChatGatewayRouteNames.ALTERNATIVE_CHANNELS,
-              component: OpenedChatGatewayWebchatAlternativeChannelsTab,
-            },{
-              path: 'recapcha',
-              name: ChatGatewayRouteNames.RE_CAPTCHA,
-              component: OpenedChatGatewayWebchatRecaptchaTab,
-            },{
-              path: 'general',
-              name: ChatGatewayRouteNames.GENERAL,
-              component: OpenedChatGatewayCustomGeneralTab,
-            },{
-              path: 'templates',
-              name: ChatGatewayRouteNames.TEMPLATES,
-              component: OpenedChatGatewayTemplatesTab,
-            },
-          ],
-        },
+
         // ----------ROUTING END------------
 
         // ----------LOOKUPS------------
---
+++ src/modules/routing/modules/flow/router/flow.js
@@ -0,0 +1,39 @@
+import RouteNames from '../../../../../app/router/_internals/RouteNames.enum.js';
+import {checkRouteAccess} from '../../../../../app/router/_internals/guards.js';
+import FlowRouteNames from './_internals/FlowRouteNames.enum.js';
+
+const Flow = () => import('../components/the-flow.vue');
+const OpenedFlow = () => import('../components/opened-flow.vue');
+const OpenedFlowDiagram = () => import('../modules/diagram/components/opened-flow-diagram.vue');
+
+const FlowRoutes= [
+  {
+    path: '/routing/flow',
+    name: RouteNames.FLOW,
+    component: Flow,
+    beforeEnter: checkRouteAccess,
+    children: [
+      {
+        path: 'uploadCSV',
+        name: FlowRouteNames.UPLOAD_CSV,
+        meta: { uploadCSV: true },
+        component: Flow,
+      }
+    ],
+  },
+  {
+    path: '/routing/flow/:id',
+    name: `${RouteNames.FLOW}-card`,
+    redirect: { name: FlowRouteNames.GENERAL },
+    component: OpenedFlow,
+    beforeEnter: checkRouteAccess,
+    children: [
+      {
+        path: 'general',
+        name: FlowRouteNames.GENERAL,
+        component: OpenedFlowDiagram,
+      }
+    ],
+  },
+];
+export default FlowRoutes;
