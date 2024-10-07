80db91175a127af4a5753870a7674127c8670163
Roman Zaritskyi <r.zaritskyi@steel.mil.ua>
2024-10-07 17:58:59 +0300

    hotfix/normalizeCSVData refactoring [WTEL-4865]

--- src/modules/_shared/upload-csv-popup/scripts/normalizeCSVData.js
+++ src/modules/_shared/upload-csv-popup/scripts/normalizeCSVData.js
@@ -1,4 +1,4 @@
-import isEmpty from "@webitel/ui-sdk/src/scripts/isEmpty";
+import isEmpty from '@webitel/ui-sdk/src/scripts/isEmpty';
 
 /**
  *
@@ -18,46 +18,35 @@ import isEmpty from "@webitel/ui-sdk/src/scripts/isEmpty";
  */
 const normalizeCSVData = ({ data, mappings }) => {
   const nonEmptyMappingFields = mappings.filter((field) => !isEmpty(field.csv));
+
   return data.map((dataItem, index) => {
     const normalized = nonEmptyMappingFields.reduce((normalizedItem, { name, csv, required }) => {
-      return {
-        ...normalizedItem,
-        [name]: {
-          required,
-          value: Array.isArray(csv) ? csv.map((csv) => dataItem[csv]) : dataItem[csv],
-        },
-      };
+      const value = Array.isArray(csv) ? csv.map((csv) => dataItem[csv]) : dataItem[csv];
+
+      let filteredValue; // Filter empty values in validation purposes
+      if (Array.isArray(value)) {
+        // Because required field can be combined from many fields in multiple select, so we need to check all values.
+        // For example, if we have 3 fields and they are empty, we will get empty array.
+        filteredValue = value.filter((item) => !isEmpty(item));
+      } else {
+        filteredValue = value;
+      }
+
+      const isValueEmpty = isEmpty(filteredValue);
+      // This check is only for required fields
+      if (required && isValueEmpty) {
+        throw new Error(`Required field is empty: ${name} on row ${index + 1}`);
+      }
+
+      return isValueEmpty
+        ? normalizedItem
+        : {
+            ...normalizedItem,
+            [name]: value, // Return the original value for proper mapping (e.g., variables in members)
+          };
     }, {});
 
-    // We need to filter empty values and check required fields for validation purposes
-    const filteredEmptyValues = Object.entries(normalized).reduce(
-      (filtered, [name, { required, value }]) => {
-        let filteredValue; // Filter empty values in validation purposes
-        if (Array.isArray(value)) {
-          // Becouse required field can be combined from many fields in multipple select, so we need to check all values.
-          // For example, if we have 3 fields and they are empty, we will get empty array.
-          filteredValue = value.filter((item) => !isEmpty(item));
-        } else {
-          filteredValue = value;
-        }
-
-        const isValueEmpty = isEmpty(filteredValue);
-        // This check is only for required fields
-        if (required && isValueEmpty) {
-          throw new Error(`Required field is empty: ${name} on row ${index + 1}`);
-        }
-
-        return isValueEmpty
-          ? filtered
-          : {
-              ...filtered,
-              [name]: value, // Need to return value, not filteredValue for proper maping (for example variables in members)
-            };
-      },
-      {}
-    );
-
-    return filteredEmptyValues;
+    return normalized;
   });
 };
 
