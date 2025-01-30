eeb6ba013a8b49a987b5f043ee99e28fb3dbec25
Daniil Lohvinov <nsblnr@gmail.com>
2025-01-30 11:52:16 +0200

    refactor: access control refactors and fixes [WTEL-5592](https://webitel.atlassian.net/browse/WTEL-5592)

--- src/modules/userinfo/userinfoStore.ts
+++ src/modules/userinfo/userinfoStore.ts
@@ -1,3 +1,3 @@
-import { createUserinfoStore } from '@webitel/ui-sdk/src/modules/Userinfo/v2/userinfoStore';
+import { createUserinfoStore } from '@webitel/ui-sdk/src/modules/Userinfo/v2/stores/userinfoStore';
 
 export const useUserinfoStore = createUserinfoStore();
