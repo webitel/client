1b8c62126b25700f6621c2c143a6553548a14e70
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-08-14 13:08:02 +0300

    refactor: icon usage [WTEL-7345](https://webitel.atlassian.net/browse/WTEL-7345)

--- src/modules/routing/modules/dialplan/components/the-dialplan.vue
+++ src/modules/routing/modules/dialplan/components/the-dialplan.vue
@@ -273,7 +273,7 @@ export default {
 <style lang="scss" scoped>
 .dialplan {
   :deep(.sortable-chosen) {
-    .dialplan__draggable-icon .wt-icon__icon {
+    .dialplan__draggable-icon .wt-icon {
       fill: var(--icon--active-color);
     }
   }
