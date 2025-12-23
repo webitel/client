0e7d4470fc02c020f2585aebf5afd3d5f4c4d6f6
Stas Kozak <kozak.stasan@gmail.com>
2025-12-23 11:01:08 +0200

    fix: implemet test changes for test inactive card [WTEL-8339](https://webitel.atlassian.net/browse/WTEL-8339)

--- src/modules/settings/components/the-settings.vue
+++ src/modules/settings/components/the-settings.vue
@@ -6,7 +6,7 @@
       </wt-page-header>
     </template>
     <template #main>
-      <section class="settings-wrapper">
+      <section class="settings-wrapper test">
         <div class="settings-wrapper-column">
           <change-password-section />
           <timezone-section />
