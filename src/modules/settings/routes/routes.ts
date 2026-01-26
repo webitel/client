74a5fd74352f70f731caad87bd2f913599d53f4c
Roman Zaritskyi <r.zaritskyi@steel.mil.ua>
2026-01-26 16:33:53 +0200

    fix: wt-tabs by using correct props  [WTEL-8736](https://webitel.atlassian.net/browse/WTEL-8736)

---
+++ src/modules/settings/routes/routes.ts
@@ -0,0 +1,34 @@
+import RouteNames from '../../../app/router/_internals/RouteNames.enum.js';
+import SettingsRouteNames from './_internals/SettingsRouteNames.enum.js';
+
+const Settings = () => import('../components/the-settings.vue');
+const GeneralSettings = () => import('../components/general-settings.vue');
+const NotificationSettings = () =>
+  import('../components/notification-settings.vue');
+
+const SettingsRoutes = [
+  {
+    path: '/settings',
+    name: RouteNames.SETTINGS_PAGE,
+    component: Settings,
+    redirect: { name: SettingsRouteNames.GENERAL },
+    meta: {
+      // prevent beforeEnter: checkAppAccess on parent route [WTEL-4321]
+      requiresAccess: false,
+    },
+    children: [
+      {
+        path: 'general',
+        name: SettingsRouteNames.GENERAL,
+        component: GeneralSettings,
+      },
+      {
+        path: 'notifications',
+        name: SettingsRouteNames.NOTIFICATIONS,
+        component: NotificationSettings,
+      },
+    ],
+  },
+];
+
+export default SettingsRoutes;
