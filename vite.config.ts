57ff5c025e1634e666f7881a6dd0a74b309cbc8a
Roman Zaritskyi <r.zaritskyi@steel.mil.ua>
2026-02-23 20:55:46 +0200

    fix: codestyle in vite config file [WTEL-8897](https://webitel.atlassian.net/browse/WTEL-8897)

--- vite.config.ts
+++ vite.config.ts
@@ -7,75 +7,67 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default () => {
-	return defineConfig({
-		server: {
+
+  return defineConfig({
+    server: {
 			// host: true,  // uncomment me to enable localhost access by IP (including from other devices in the network)
-		},
-		css: {
-			preprocessorOptions: {
-				scss: {
-					api: 'modern-compiler',
-					additionalData: `
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+          additionalData: `
             // @use "@/app/css/reusable.scss" as *;
           `,
-				},
-			},
-		},
-		resolve: {
-			alias: {
-				vue: '@vue/compat',
-				'@': resolve(__dirname, 'src'),
-				'@aliasedDeps/api-services/axios': resolve(
-					__dirname,
-					'src/app/api/instance',
-				),
-			},
-			dedupe: [
-				'vue',
-				'@vue/compat',
-			],
-		},
-		plugins: [
-			vue({
-				template: {
-					compilerOptions: {
-						compatConfig: {
-							MODE: 2,
-						},
-					},
-				},
-			}),
-			// https://www.npmjs.com/package/vite-plugin-node-polyfills
-			nodePolyfills({
-				// are needed for csv-parse
-				include: [
-					'buffer',
-					'stream',
-					'process',
-				],
-				globals: {
-					Buffer: true, // can also be 'build', 'dev', or false
-					process: true, // csv stringify
-				},
-			}),
-			vueDevTools({
-				launchEditor: 'webstorm',
-			}),
-			checker({
-				typescript: false,
-				vueTsc: false,
-			}),
-		],
-		test: {
-			globals: true,
-			coverage: {
-				enabled: true,
-				reporter: 'json',
-			},
-			environment: 'happy-dom',
-			setupFiles: [
-				'./tests/config/config.js',
-			],
-		},
-	});
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        vue: '@vue/compat',
+        '@': resolve(__dirname, 'src'),
+        '@aliasedDeps/api-services/axios': resolve(__dirname, 'src/app/api/instance'),
+      },
+      dedupe: ['vue', '@vue/compat'],
+    },
+    optimizeDeps: {
+      exclude: ['@webitel/styleguide'],
+    },
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2,
+            },
+          },
+        },
+      }),
+      // https://www.npmjs.com/package/vite-plugin-node-polyfills
+      nodePolyfills({
+        // are needed for csv-parse
+        include: ['buffer', 'stream', 'process'],
+        globals: {
+          Buffer: true, // can also be 'build', 'dev', or false
+          process: true, // csv stringify
+        },
+      }),
+      vueDevTools({
+        launchEditor: 'webstorm',
+      }),
+      checker({
+        typescript: false,
+        vueTsc: false,
+      }),
+    ],
+    test: {
+      globals: true,
+      coverage: {
+        enabled: true,
+        reporter: 'json',
+      },
+      environment: 'happy-dom',
+      setupFiles: ['./tests/config/config.js'],
+    },
+  });
 };
