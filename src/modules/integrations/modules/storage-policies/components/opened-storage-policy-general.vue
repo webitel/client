bab7126049d277a4877f3ed5184e823f307b97b9
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2026-07-20 15:57:53 +0300

    fix(policy card): Changed mime type select options ([WTEL-9925](https://webitel.atlassian.net/browse/WTEL-9925))

--- src/modules/integrations/modules/storage-policies/components/opened-storage-policy-general.vue
+++ src/modules/integrations/modules/storage-policies/components/opened-storage-policy-general.vue
@@ -79,7 +79,7 @@
           chips-view
           allow-custom-values
           required
-          :options="MimeTypes"
+          :options="mimeTypeOptions"
           :data-key="null"
           @update:model-value="setItemProp({ prop: 'mimeTypes', value: $event })"
         />
@@ -189,6 +189,9 @@ export default {
 					value: this.snakeToCamel(channel),
 				}));
 		},
+    mimeTypeOptions() {
+      return [...new Set([...this.MimeTypes, ...this.itemInstance.mimeTypes])];
+    }
 	},
 	methods: {
 		snakeToCamel,
