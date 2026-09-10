572d7c961f45e269c69543e9264f4ea8b93544ef
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2026-09-10 13:31:10 +0300

    feat(agent-pause-cause): set column min-widths for agent statuses table [WTEL-10341](https://webitel.atlassian.net/browse/WTEL-10341)

--- src/modules/lookups/modules/agent-pause-cause/store/_internals/headers.js
+++ src/modules/lookups/modules/agent-pause-cause/store/_internals/headers.js
@@ -4,12 +4,14 @@ export default [
 	{
 		value: 'name',
 		locale: 'objects.name',
+		width: '250px',
 		field: 'name',
 		sort: SortSymbols.NONE,
 	},
 	{
 		value: 'limit',
 		locale: 'objects.lookups.pauseCause.limit',
+		width: '140px',
 		field: 'limit_min',
 		sort: SortSymbols.NONE,
 	},
@@ -23,18 +25,21 @@ export default [
 	{
 		value: 'allowAdmin',
 		locale: 'objects.lookups.pauseCause.allowAdmin',
+		width: '200px',
 		field: 'allow_admin',
 		sort: SortSymbols.NONE,
 	},
 	{
 		value: 'allowSupervisor',
 		locale: 'objects.lookups.pauseCause.allowSupervisor',
+		width: '235px',
 		field: 'allow_supervisor',
 		sort: SortSymbols.NONE,
 	},
 	{
 		value: 'allowAgent',
 		locale: 'objects.lookups.pauseCause.allowAgent',
+		width: '195px',
 		field: 'allow_agent',
 		sort: SortSymbols.NONE,
 	},
